ac755129f25f4292df3d172c1291008752ed605e
Dave Doolin <david.doolin@gmail.com>
2012-04-10 15:12:09 -0700

    Compiling Boost unit test.

--- test/test_readtext.cpp
+++ test/test_readtext.cpp
@@ -1,7 +1,23 @@
 #include <boost/test/auto_unit_test.hpp>
+using namespace boost::unit_test;
+
+void
+my_test_function() {
+  BOOST_CHECK(1==1);
+}
+
+test_suite*
+init_unit_test_suite( int argc, char* argv[] ) {
+
+  test_suite * test = BOOST_TEST_SUITE( "Master test suite" );
+  test->add( BOOST_TEST_CASE( &my_test_function ) );
+  return test;
+}
+
 
 int
 main(int argc, char ** argv) {
 
+  init_unit_test_suite(argc, argv);
   return 0;
 }
