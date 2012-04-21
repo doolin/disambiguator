e2d6371dfda9580134c5fc63b29f2118a22a1e3b
Dave Doolin <david.doolin@gmail.com>
2012-04-21 15:46:53 -0400

    first unit test demo with cpp unit and autotools compiles, links and runs

---
+++ test/test_initial.cpp
@@ -0,0 +1,31 @@
+
+#include <cppunit/Portability.h>
+#include <cppunit/portability/CppUnitSet.h>
+#include <cppunit/extensions/TestFactory.h>
+#include <cppunit/TestCase.h>
+#include <string>
+
+
+class Initial {
+public:
+  Initial(void) : value(1) {}
+  int value;
+};
+
+class InitialTest : public CppUnit::TestCase { 
+public: 
+  InitialTest( std::string name ) : CppUnit::TestCase( name ) {}
+ 
+  void runTest() {
+    Initial * i = new Initial();
+    CPPUNIT_ASSERT( i->value  == 2 );
+  }
+};
+
+int
+main(int argc, char ** argv) {
+
+  InitialTest * it = new InitialTest(std::string("initial test"));
+  it->runTest();
+  return 0;
+}
