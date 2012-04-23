9e0638540f7e07cfcf143f4e6c9720122e4faa35
Dave Doolin <david.doolin@gmail.com>
2012-04-23 11:52:28 -0400

    forcing command line options

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
 
 int disambiguate_main(void);
 
 int
 main(int argc, char ** argv) {
+
+  if (argc < 3) {
+    std::cout << "usage: disambiguate <engine_conf> <block_conf>" << std::endl;
+    exit(0);
+  }
 
   return disambiguate_main();
 }
