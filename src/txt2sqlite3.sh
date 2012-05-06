aad8cd6298b30406dbe7f35c7a1a0c9508972463
Dave Doolin <david.doolin@gmail.com>
2012-05-05 21:20:14 -0400

    stubbed a driver file for converting final results txt files back into sqlite files.

---
+++ src/txt2sqlite3.sh
@@ -0,0 +1,5 @@
+
+# This will be a driver file for the txt2sqlite convertor
+
+# ./txt2sqlite3 /var/share/patentdata/disambiguation/test/18/txt2sqlite3.sqlite3 final /var/share/patentdata/disambiguation/test/18/final.txt    UNIQUE_RECORD_COLUMN_NAME UNIQUE_INVENTOR_COLUMN_NAME
+
