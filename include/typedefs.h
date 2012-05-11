192019b7857674be76e3249727c17fce8cdcced8
Dave Doolin <david.doolin@gmail.com>
2012-05-10 23:36:48 -0400

    added a note about moving SimilarityProfiles back to ratios and smoothing after those are cleaned up.

--- include/typedefs.h
+++ include/typedefs.h
@@ -7,6 +7,9 @@
 
 typedef std::vector<std::string> Label;
 
+// SimilarityProfile will probably get moved back
+// to the ratios and smoothing code once that us
+// cleaned up and refactored more.
 typedef std::vector < unsigned int > SimilarityProfile;
 
 #endif /* PATENT_TYPEDEFS_H */
