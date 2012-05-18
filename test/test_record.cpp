9c6f9233300cf84eca1e09276fbd726fcbaa93d5
Dave Doolin <david.doolin@gmail.com>
2012-05-18 00:01:26 -0400

    working on attribute constructor for record

--- test/test_record.cpp
+++ test/test_record.cpp
@@ -1,21 +1,27 @@
+#include <string>
+#include <vector>
 
 #include <cppunit/Portability.h>
 #include <cppunit/portability/CppUnitSet.h>
 #include <cppunit/extensions/TestFactory.h>
 #include <cppunit/TestCase.h>
-#include <string>
-
 
 #include <disambiguation.h>
-
 #include <DisambigEngine.h>
-
-
 
 class RecordTest : public CppUnit::TestCase { 
 
 public: 
   RecordTest( std::string name ) : CppUnit::TestCase( name ) {}
+
+  void attribute_record() {
+    const cFirstname * firstname = new cFirstname();
+    //Record(const vector <const Attribute *> & input_vec);
+    //const std::vector <const Attribute *> input_vec;
+    //input_vec.push_back(firstname);
+    //Record r(input_vec);
+    delete firstname;
+  }
 
   void delete_record() {
     Record * rc = new Record();
@@ -26,6 +32,7 @@ public:
     // Just o get startes...
     CPPUNIT_ASSERT( 1  == 1 );
     delete_record();
+    attribute_record();
   }
 };
 
