53972939823ac7fb276be041f1c38a06baa47481
Dave Doolin <david.doolin@gmail.com>
2012-05-19 13:12:14 -0400

    added string remove space unit test

---
+++ test/test_string_manipulator.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+
+#include <cppunit/Portability.h>
+#include <cppunit/portability/CppUnitSet.h>
+#include <cppunit/extensions/TestFactory.h>
+#include <cppunit/TestCase.h>
+
+#include <disambiguation.h>
+#include <DisambigEngine.h>
+
+
+class StringManipulatorTest : public CppUnit::TestCase { 
+
+public: 
+  StringManipulatorTest( std::string name ) : CppUnit::TestCase( name ) {}
+
+  void remove_space() {
+    std::string input("THIS IS AN   EXAMPLE  ");
+    std::string target("THISISANEXAMPLE");
+    cString_Remove_Space * rc = new cString_Remove_Space();
+    std::string result = rc->manipulate(input);
+    CPPUNIT_ASSERT(result == target);
+    delete rc;
+  }
+ 
+  void runTest() {
+    CPPUNIT_ASSERT( 1  == 1 );
+    remove_space();
+  }
+};
+
+int
+main(int argc, char ** argv) {
+
+  StringManipulatorTest * rt = new StringManipulatorTest(std::string("initial test"));
+  rt->runTest();
+  delete rt;
+  return 0;
+}
