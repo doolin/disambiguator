7659477aebf5795bf74fa7cfabbf94f33702f07d
Dave Doolin <david.doolin@gmail.com>
2012-05-19 13:44:54 -0400

    removed tabs

--- include/string_manipulator.h
+++ include/string_manipulator.h
@@ -6,12 +6,12 @@
 
 /*
  * cString_Manipulator:
- * 	- cString_Remain_Same
- * 	- cString_Remove_Space
- * 	- cString_Truncate
- * 		-- cString_NoSpace_Truncate
- * 	- cExtract_Initials
- * 	- cString_Extract_FirstWord
+ *     - cString_Remain_Same
+ *     - cString_Remove_Space
+ *     - cString_Truncate
+ *         -- cString_NoSpace_Truncate
+ *     - cExtract_Initials
+ *     - cString_Extract_FirstWord
  *
  * cString_Manipulator is a hierarchy of of string operation functors. The reason to create such hierarchy is to allow polymorphism.
  * Subclasses override the virtual function "string manipulate ( const string & input ) const " with their own implementations.
@@ -25,12 +25,12 @@ class cString_Manipulator{
 private:
 
 public:
-	// Not the right way to do this. See, for example:
-	// http://steve-yegge.blogspot.com/2006/03/execution-in-kingdom-of-nouns.html
-	// Substitute "manipulate" for "execute" or "run" and there you have it.
-	virtual string manipulate(const string & inputstring) const = 0;
-	virtual ~cString_Manipulator() {};
-	virtual cString_Manipulator * clone () const = 0;
+    // Not the right way to do this. See, for example:
+    // http://steve-yegge.blogspot.com/2006/03/execution-in-kingdom-of-nouns.html
+    // Substitute "manipulate" for "execute" or "run" and there you have it.
+    virtual string manipulate(const string & inputstring) const = 0;
+    virtual ~cString_Manipulator() {};
+    virtual cString_Manipulator * clone () const = 0;
 };
 
 /*
@@ -41,9 +41,9 @@ public:
 
 class cString_Remain_Same : public cString_Manipulator {
 private:
-	cString_Manipulator * clone () const { return new cString_Remain_Same(*this);}
+    cString_Manipulator * clone () const { return new cString_Remain_Same(*this);}
 public:
-	string manipulate(const string & inputstring ) const { return inputstring;}
+    string manipulate(const string & inputstring ) const { return inputstring;}
 
 };
 
@@ -57,15 +57,15 @@ public:
 
 class cString_Remove_Space : public cString_Manipulator {
 private:
-	static const char delimiter = ' ';
-	cString_Manipulator * clone () const { return new cString_Remove_Space(*this);}
+    static const char delimiter = ' ';
+    cString_Manipulator * clone () const { return new cString_Remove_Space(*this);}
 public:
-	string manipulate( const string & inputstring ) const {
-		string result = inputstring;
-		result.erase(std::remove_if (result.begin(), result.end(), ::isspace), result.end() );
-		return result;
-	};
-	explicit cString_Remove_Space(){};
+    string manipulate( const string & inputstring ) const {
+        string result = inputstring;
+        result.erase(std::remove_if (result.begin(), result.end(), ::isspace), result.end() );
+        return result;
+    };
+    explicit cString_Remove_Space(){};
 };
 
 /*
@@ -74,7 +74,7 @@ public:
  *
  * Private:
  * int begin: location of the starting point for truncation. Non-negative value means the position of starting character is from the beginning,
- * 			  and negative value means the position of starting character is from the end of the string.
+ *               and negative value means the position of starting character is from the end of the string.
  * unsigned int nchar: number of characters to extract. if nchar == 0, all the string will be extracted. If nchar > length of string, only the string part will be kept.
  * bool is_forward: indicating the direction to truncation. true = forward ( left to right ), false = backward ( right to left )
  * bool is_usable: internal data of the class. false = the object is not prepared yet. true = ready to use.
@@ -86,7 +86,7 @@ public:
  *
  *
  * Public:
- * 	explicit cString_Truncate(): default constructor.
+ *     explicit cString_Truncate(): default constructor.
  *  void set_truncater( const int inputbegin, const unsigned int inputnchar, const bool inputforward): configure the object.
  *  string manipulate( const string & inputstring ) const: implementation of the extraction operation. Defined in cpp file.
  *
@@ -114,25 +114,25 @@ public:
 
 class cString_Truncate: public cString_Manipulator {
 private:
-	int begin;
-	unsigned int nchar;
-	bool is_forward;
-	bool is_usable;
+    int begin;
+    unsigned int nchar;
+    bool is_forward;
+    bool is_usable;
 
-	class cException_String_Truncation: public cAbstract_Exception {
-	public:
-		cException_String_Truncation (const char * errmsg) : cAbstract_Exception(errmsg) {};
-	};
-	cString_Manipulator * clone () const { return new cString_Truncate(*this);}
+    class cException_String_Truncation: public cAbstract_Exception {
+    public:
+        cException_String_Truncation (const char * errmsg) : cAbstract_Exception(errmsg) {};
+    };
+    cString_Manipulator * clone () const { return new cString_Truncate(*this);}
 public:
-	explicit cString_Truncate(): is_usable (false) {};
-	void set_truncater( const int inputbegin, const unsigned int inputnchar, const bool inputforward) {
-		begin = inputbegin;
-		nchar = inputnchar;
-		is_forward = inputforward;
-		is_usable = true;
-	}
-	string manipulate( const string & inputstring ) const;
+    explicit cString_Truncate(): is_usable (false) {};
+    void set_truncater( const int inputbegin, const unsigned int inputnchar, const bool inputforward) {
+        begin = inputbegin;
+        nchar = inputnchar;
+        is_forward = inputforward;
+        is_usable = true;
+    }
+    string manipulate( const string & inputstring ) const;
 };
 
 /*
@@ -144,13 +144,13 @@ public:
 
 class cString_NoSpace_Truncate: public cString_Truncate {
 private:
-	const cString_Remove_Space ns;
-	cString_Manipulator * clone () const { return new cString_NoSpace_Truncate(*this);}
+    const cString_Remove_Space ns;
+    cString_Manipulator * clone () const { return new cString_NoSpace_Truncate(*this);}
 public:
-	string manipulate ( const string & inputstring ) const {
-		string temp = ns.manipulate(inputstring);
-		return cString_Truncate::manipulate(temp);
-	}
+    string manipulate ( const string & inputstring ) const {
+        string temp = ns.manipulate(inputstring);
+        return cString_Truncate::manipulate(temp);
+    }
 };
 
 
@@ -159,19 +159,19 @@ public:
  * Extract the initials from the string, starting from the (starting_word)th word.
  *
  * Example:
- * cExtract_Initials eiobj(3);	//create an instance eiobj, setting the starting_word = 3;
+ * cExtract_Initials eiobj(3);    //create an instance eiobj, setting the starting_word = 3;
  * eiobj.manipulate("THIS IS AN EXAMPLE, YOU KNOW.") returns "EYK".
  *
  */
 
 class cExtract_Initials : public cString_Manipulator {
 private:
-	const unsigned int starting_word;
-	static const char delimiter = ' ';
-	cString_Manipulator * clone () const { return new cExtract_Initials(*this);}
+    const unsigned int starting_word;
+    static const char delimiter = ' ';
+    cString_Manipulator * clone () const { return new cExtract_Initials(*this);}
 public:
-	string manipulate (const string & inputstring ) const;
-	explicit cExtract_Initials (const unsigned int start ): starting_word(start) {};
+    string manipulate (const string & inputstring ) const;
+    explicit cExtract_Initials (const unsigned int start ): starting_word(start) {};
 };
 
 
@@ -185,10 +185,10 @@ public:
 
 class cString_Extract_FirstWord : public cString_Manipulator {
 private:
-	static const char delimiter = ' ';
-	cString_Manipulator * clone () const { return new cString_Extract_FirstWord(*this);}
+    static const char delimiter = ' ';
+    cString_Manipulator * clone () const { return new cString_Extract_FirstWord(*this);}
 public:
-	string manipulate (const string & inputstring ) const;
+    string manipulate (const string & inputstring ) const;
 };
 
 
