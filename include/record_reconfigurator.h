5a6daa0aaadc08c4640da010a5235a1b6de23a95
Dave Doolin <david.doolin@gmail.com>
2012-06-10 13:43:47 -0700

    refactor record_reconfigurator classes.

---
+++ include/record_reconfigurator.h
@@ -0,0 +1,171 @@
+
+
+// Record_Reconfigurator classes
+
+
+/**
+ * Record_Reconfigurator
+ *     - cReconfigurator_AsianNames
+ *     - cReconfigurator_Latitude_Interactives
+ *     - cReconfigurator_Coauthor
+ *
+ * The record reconfigurator hierarchy is used to modify
+ * the data after initial loading from text files.
+ * For some reasons, initial data are not ideal for disambiguation,
+ * and modification of such data is necessary.
+ * It is also critical to reconfigure Record objects when
+ * interactions between attributes exist.
+ * To add more concrete classes, simply inherit the Record_Reconfigurator
+ * class and implement the "void reconfigure ( const Record * ) const" virtual function.
+ * To use a concrete Record_Reconfigurator class, create an object of
+ * the class and treat the object as a function, as shown below.
+ *
+ */
+class Record_Reconfigurator {
+public:
+    virtual void reconfigure ( const Record * ) const = 0;
+    void operator () ( Record & r ) const  {this->reconfigure(&r);}
+    void operator () ( const Record * p) const { this->reconfigure(p); }
+    virtual ~Record_Reconfigurator(){}
+};
+
+
+/**
+ * cReconfigurator_AsianNames:
+ * This class is specifically designed for east asian names. Generally,
+ * east asian names do not have middle names. For example,
+ * "MING LIANG.ZHANG" of China has the first name as "MING LIANG"
+ * instead of "MING", whereas the originally regarded "LIANG" as
+ * middle name actually is not middle name. Moreover, their first
+ * names and last names are usually extremely similar in terms of spelling.
+ * Therefore, to accommodate reasonable comparisons, their first names are
+ * set to be the original strings from the raw text data,
+ * and their middle names are set to be the combination of first
+ * names and last names.
+ *
+ * Private:
+ *         const unsigned int country_index: index of country column in the record vector
+ *         const unsigned int firstname_index: index of first name column in the record vector
+ *         const unsigned int middlename_index: index of middle name column in the record vector
+ *         const unsigned int lastname_index: index of last name column in the record vector
+ *         vector < string > east_asian: string of east asian country ids.
+ *         const StringRemoveSpace rmvsp: a sring manipulator. Perhaps not used here.
+ *
+ * Public:
+ *         cReconfigurator_AsianNames(): constructor
+ *         void reconfigure( const Record * ) const: virtual function.
+ *
+ *
+ * For example: assuming there is a record object, recobj, which has the
+ * cFirstname attribute, cMiddlename and cLastname attribute shown as below.
+ *
+ * Before reconfiguration:
+ *  1. const Attribute * pfirstname: pointing to an attribute whose data vector contains 2 string pointers:
+ *         1.1 pfirstname->data[0] points to "MING LIANG"
+ *         1.2 pfirstname->data[1] points to "MING"
+ *
+ *  2. const Attribute * pmiddlename: pointing to an attribute whose data vector contains 2 string pointers:
+ *         2.1 pfirstname->data[0] points to "MING LIANG"
+ *         2.2 pfirstname->data[1] points to "LIANG"
+ *
+ *  3. const Attribute * plasname: pointing to an attribute whose data vector contains 2 string pointers:
+ *         3.1 pfirstname->data[0] points to "ZHANG"
+ *         3.2 pfirstname->data[1] points to "ZHANG"
+ *
+ *  By applying the reconfiguration through:
+ *  cReconfigurator_AsianNames rcfgobj; //create a reconfigurator object.
+ *  rcfgobj ( & recobj ); // rcfgobj is a cReconfigurator_AsianNames object
+ *  We now have:
+ *
+ *  After reconfiguration:
+ *  1. const Attribute * pfirstname: pointing to an attribute whose data vector contains 2 string pointers:
+ *         1.1 pfirstname->data[0] points to "MING LIANG"
+ *         1.2 pfirstname->data[1] points to "MING LIANG"
+ *
+ *  2. const Attribute * pmiddlename: pointing to an attribute whose data vector contains 2 string pointers:
+ *         2.1 pfirstname->data[0] points to "MING LIANG.ZHANG"
+ *         2.2 pfirstname->data[1] points to "MING LIANG.ZHANG"
+ *
+ *  3. const Attribute * plasname: pointing to an attribute whose data vector contains 2 string pointers:
+ *         3.1 pfirstname->data[0] points to "ZHANG"
+ *         3.2 pfirstname->data[1] points to "ZHANG"
+ *
+ */
+class cReconfigurator_AsianNames : public Record_Reconfigurator {
+
+private:
+    const unsigned int country_index;
+    const unsigned int firstname_index;
+    const unsigned int middlename_index;
+    const unsigned int lastname_index;
+    vector < string > east_asian;
+    const StringRemoveSpace rmvsp;
+
+public:
+    cReconfigurator_AsianNames();
+    void reconfigure( const Record * ) const;
+};
+
+
+
+/**
+ * cReconfigurator_Interactives:
+ * This class is used to modify the objects to connect with its associated other Attribute objects.
+ *
+ * Example:
+ * vector < string > links;
+ * links.push_back("Longitude");
+ * links.push_back("Street");
+ * links.push_back("Country");
+ * cReconfigurator_Interactives obj ("Latitude", links);    // get an instance
+ * obj ( & recobj );    // internal modification is done.
+ * obj.reconfigure ( & recobj ); //the same as above;
+ */
+class cReconfigurator_Interactives: public Record_Reconfigurator {
+
+private:
+    unsigned int my_index;
+    vector < unsigned int > relevant_indice;
+public:
+    cReconfigurator_Interactives(const string & my_name, const vector <string> & relevant_attributes);
+    void reconfigure ( const Record *) const;
+};
+
+
+/**
+ * cReconfigurator_Coauthor:
+ * This class is primarily used to clean the data obtained from loading from the Coauthor column.
+ * Since our coauthor column is not clean enough ( with some legacy expression ), this reconfigurator discards the information from
+ * the loaded coauthor column and builds a clean one.
+ * Therefore, this class should be used very cautiously, as it wipes everything about coauthor WHENEVER an object of such class is created.
+ *
+ * Private:
+ *         const map < const Record *, cGroup_Value, cSort_by_attrib > * reference_pointer: a pointer to a patent tree, which can be obtained in a cBlocking_Operation_By_Coauthor object.
+ *         const unsigned int coauthor_index: the index of the coauthor column in many columns.
+ *
+ * Public:
+ *         cReconfigurator_Coauthor ( const map < const Record *, cGroup_Value, cSort_by_attrib > & patent_authors):
+ *             create a class object through a patent tree ( which is usually from a cBlocking_Operation_By_Coauthor object ).
+ *         void reconfigure ( const Record * ) const: virtual function.
+ *
+ *
+ * Example:
+ * list < const Record * > complete; // create a complete list of Record objects
+ * Do something to fill in the list;
+ * cBlocking_Operation_By_Coauthor bobcobj( complete, 1 ); // create a cBlocking_Operation_By_Coauthor object.
+ * cReconfigurator_Coauthor rcobj ( bobcobj.get_patent_tree() );    //create an object of coauthor reconfigurator. This also wipes everything of the whole coauthor column!!
+ * std::for_each ( complete.begin(), complete.end(), rcobj ); // this rebuilds the coauthor for each patent.
+ *
+ *
+ */
+class cReconfigurator_Coauthor : public Record_Reconfigurator {
+
+private:
+    const map < const Record *, cGroup_Value, cSort_by_attrib > * reference_pointer;
+    const unsigned int coauthor_index;
+public:
+    cReconfigurator_Coauthor ( const map < const Record *, cGroup_Value, cSort_by_attrib > & patent_authors);
+    void reconfigure ( const Record * ) const;
+};
+
+
--- src/engine.cpp
+++ src/engine.cpp
@@ -24,144 +24,12 @@ using std::set;
 
 
 
-/**
- * Aim: constructor of cReconfigurator_AsianNames.
- */
-cReconfigurator_AsianNames::cReconfigurator_AsianNames(): country_index(Record::get_index_by_name(cCountry::static_get_class_name())),
-                                firstname_index(Record::get_index_by_name(cFirstname::static_get_class_name())),
-                                middlename_index(Record::get_index_by_name(cMiddlename::static_get_class_name())),
-                                lastname_index(Record::get_index_by_name(cLastname::static_get_class_name())){
-    east_asian.push_back(string("KR"));
-    east_asian.push_back(string("CN"));
-    east_asian.push_back(string("TW"));
-}
-
-
-/**
- * Aim: reconfigure the asian name in accordance with the current scoring system.
- *
- * Algorithm: check the country first. If the country is in [ "KR", "CN", "TW"],
- * do this operation:
- *  1. Create a vector of string, and save FULL first name into the vector. 
- *     Then create a firstname attribute from the vector
- *  2. Create a vector of string, and save "FULL firstname" + "FULL lastname"
- *     into the vector. Then create a middlename attribute from the vector.
- *  3. Change the Record data such that the firstname pointer points to the
- *     newly created firstname object. So for the middle name.
- */
-void
-cReconfigurator_AsianNames::reconfigure( const Record * p ) const {
-
-    bool need_reconfigure = false;
-    const string & country = *  p->get_attrib_pointer_by_index(country_index)->get_data().at(0) ;
-
-    for ( register vector<string>::const_iterator ci = east_asian.begin(); ci != east_asian.end(); ++ci )
-        if ( country == *ci ) {
-            need_reconfigure = true;
-            break;
-        }
-
-    if (need_reconfigure == false) return;
-
-    // do not change original attributes. add new ones.
-    const Attribute * cur_af = p->get_attrib_pointer_by_index(firstname_index);
-    const string & fn_alias = * cur_af ->get_data().at(0);
-    const vector <string> fn ( 2, fn_alias );
-    const Attribute * paf = cFirstname::static_clone_by_data(fn);
-
-    const Attribute * cur_am = p->get_attrib_pointer_by_index(middlename_index);
-    const string & lnstr = * p->get_attrib_pointer_by_index(lastname_index)->get_data().at(0);
-    const string mnstr ( fn_alias + "." + lnstr);
-    const vector < string > mn(2, mnstr);
-    const Attribute * pam = cMiddlename ::static_clone_by_data(mn);
-
-    Record * q = const_cast < Record * > (p);
-
-    cur_af->reduce_attrib(1);
-    cur_am->reduce_attrib(1);
-    q->set_attrib_pointer_by_index(paf, firstname_index);
-    q->set_attrib_pointer_by_index(pam, middlename_index);
-}
-
-
-cReconfigurator_Interactives::cReconfigurator_Interactives( const string & my_name,
-        const vector < string > & relevant_attribs ) {
-    my_index = Record::get_index_by_name(my_name);
-    for ( vector<string>::const_iterator p = relevant_attribs.begin(); p != relevant_attribs.end(); ++p ) {
-        unsigned int idx = Record::get_index_by_name(*p);
-        relevant_indice.push_back(idx);
-    }
-}
-
-
-void
-cReconfigurator_Interactives::reconfigure ( const Record * p ) const {
-
-    vector < const Attribute * > interact;
-    for ( vector < unsigned int >::const_iterator i = relevant_indice.begin(); i != relevant_indice.end(); ++i ) {
-        interact.push_back(p->get_attrib_pointer_by_index(*i));
-    }
-    const Attribute * const & tp = p->get_attrib_pointer_by_index(my_index);
-    const Attribute * & cp = const_cast< const Attribute * &> (tp);
-    cp = tp->config_interactive(interact);
-}
-
-
-/*
- * Aim: constructor of cReconfigurator_Coauthor
- * Algorithm: The constructor calls cCoauthor::clear_data_pool() and cCoauthor::clear_attrib_pool(). So it is critical to remember that
- * construction of each cReconfigurator_Coauthor object will DESTROY all the information of any cCoauthor class and INVALIDATE any pointers
- * that point to a cCoauthor object. Therefore, creation of cReconfigurator_Coauthor objects shall NEVER happen during disambiguation.
- *
- */
-cReconfigurator_Coauthor::cReconfigurator_Coauthor ( const map < const Record *, cGroup_Value, cSort_by_attrib > & patent_authors) :
-        reference_pointer ( & patent_authors), coauthor_index ( Record::get_index_by_name(cCoauthor::static_get_class_name())) {
-    cCoauthor::clear_data_pool();
-    cCoauthor::clear_attrib_pool();
-}
-
-
-/*
- * Aim: to recreate the whole coauthor database, and link them to appropriate pointers.
- * Algorithm: for each unique record id, find all other unique id records with which this one is associated. Then extract the FULL names
- * of those records, and save in cCoauthor data pool. After that, reset the pointer of this unique record to point to the newly built
- * attribute.
- */
-
-void cReconfigurator_Coauthor :: reconfigure ( const Record * p ) const {
-    static const string dot = ".";
-    static const unsigned int firstnameindex = Record::get_index_by_name(cFirstname::static_get_class_name());
-    static const unsigned int lastnameindex = Record::get_index_by_name(cLastname::static_get_class_name());
-    static const StringExtractFirstWord firstname_extracter;
-    static const StringRemoveSpace lastname_extracter;
-
-    map < const Record *, cGroup_Value, cSort_by_attrib >::const_iterator cpm;
-    cCoauthor temp;
-
-    cpm = reference_pointer->find( p);
-    if ( cpm == reference_pointer->end())
-        throw cException_Other("Missing patent data.");
-    const cGroup_Value & patent_coauthors = cpm->second;
-    for ( cGroup_Value::const_iterator q = patent_coauthors.begin(); q != patent_coauthors.end(); ++q ) {
-        if ( *q == p )
-            continue;
-        string fullname = firstname_extracter.manipulate( * (*q)->get_data_by_index(firstnameindex).at(0) ) + dot
-                            + lastname_extracter.manipulate( * (*q)->get_data_by_index(lastnameindex).at(0) );
-        temp.attrib_set.insert(cCoauthor::static_add_string (fullname) );
-    }
-    const Attribute * np = cCoauthor::static_add_attrib(temp, 1);
-    const Attribute ** to_change = const_cast< const Attribute ** > ( & p->get_attrib_pointer_by_index(coauthor_index));
-    *to_change = np;
-
-}
-
-
 /*
  * Aim: to find a ratio that corresponds to a given similarity
  * profile in a given similarity profile binary tree.
  * Algorithm: STL map find.
  */
-double 
+double
 fetch_ratio(const vector < unsigned int > & ratio_to_lookup,
             const map < vector  < unsigned int>,
             double, cSimilarity_Compare > & ratiosmap ) {
@@ -174,7 +42,7 @@ fetch_ratio(const vector < unsigned int > & ratio_to_lookup,
 }
 
 
-std::pair<const Record *, double> 
+std::pair<const Record *, double>
 disambiguate_by_set (const Record * key1,
                      const cGroup_Value & match1,
                      const double cohesion1,
@@ -303,7 +171,7 @@ disambiguate_by_set (const Record * key1,
 }
 
 
-void 
+void
 copyfile(const char * target, const char * source) {
 
     std::cout << "Copying file " << source << " to " << target << std::endl;
@@ -636,17 +504,6 @@ create_attribute_instance ( const string & id ) {
         p = NULL;
 
     return p;
-}
-
-
-const
-Record_Reconfigurator * generate_interactive_reconfigurator( const Attribute * pAttrib) {
-
-    vector <string > linked_attribs (pAttrib->get_interactive_class_names());
-    string my_name = pAttrib->get_class_name();
-    //ATTENTION: OBJECT IS ON HEAP.
-    const Record_Reconfigurator * preconfig = new cReconfigurator_Interactives( my_name, linked_attribs );
-    return preconfig;
 }
 
 
