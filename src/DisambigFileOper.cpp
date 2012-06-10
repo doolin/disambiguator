8380527935df65780dd57beb499393bf80f90d56
Dave Doolin <david.doolin@gmail.com>
2012-06-10 07:22:39 -0700

    cleanup for readability

--- src/DisambigFileOper.cpp
+++ src/DisambigFileOper.cpp
@@ -1,17 +1,15 @@
-/*
- *  DisambigFileOper.cpp
- *  mydisambiguation
- */
 
-#include "DisambigFileOper.h"
-#include "DisambigEngine.h"
-//#include "sqlite3op.h"
-#include "DisambigCluster.h"
 #include <fstream>
 #include <cstring>
 
-const Record * 
+#include "DisambigFileOper.h"
+#include "DisambigEngine.h"
+#include "DisambigCluster.h"
+
+
+const Record *
 retrieve_record_pointer_by_unique_id(const string & uid, const map <string, const Record*> & uid_tree) {
+
     map <string, const Record *>::const_iterator cpm = uid_tree.find(uid);
     if ( cpm == uid_tree.end())
         throw cException_Attribute_Not_In_Tree(uid.c_str());
@@ -20,11 +18,15 @@ retrieve_record_pointer_by_unique_id(const string & uid, const map <string, cons
 }
 
 
-void 
-create_btree_uid2record_pointer(map<string, const Record *> & uid_tree, const list<Record> & reclist, const string& uid_name ) {
+void
+create_btree_uid2record_pointer(map<string, const Record *> & uid_tree,
+                                const list<Record> & reclist,
+                                const string& uid_name ) {
+
     uid_tree.clear();
     const unsigned int uid_index = Record::get_index_by_name(uid_name);
     cException_Vector_Data except(uid_name.c_str());
+
     map <string, const Record *>::iterator pm;
     for ( list<Record>::const_iterator p = reclist.begin(); p != reclist.end(); ++p ) {
         const Attribute * pattrib = p->get_attrib_pointer_by_index(uid_index);
