6b4419f342d2ab80ac8e3290d8ddf32a49de33e5
Dave Doolin <david.doolin@gmail.com>
2012-06-10 18:55:04 -0700

    Getting ready to remove DisambigEngine

--- include/DisambigEngine.h
+++ include/DisambigEngine.h
@@ -1,44 +1,8 @@
-/*
- * DisambigEngine.h
- *
- *  Created on: Dec 13, 2010
- *      Author: ysun
- */
 
 #ifndef DISAMBIGENGINE_H_
 #define DISAMBIGENGINE_H_
 
-#include <iostream>
-#include <list>
-#include <vector>
-#include <string>
-#include <map>
-#include <set>
-#include <algorithm>
 
-#include "attribute.h"
-#include "Threading.h"
-
-using std::string;
-using std::list;
-using std::vector;
-using std::map;
-using std::set;
-
-// forward declarations
-//class cBlocking_Operation_By_Coauthors;
-
-
-// cGroup_Value is a list of const Record pointers.
-// This definition will be used throughout the whole project.
-// TODO: This is ludicrous that Record can be used in a typedef
-// before it's defined in its header file below. This means
-// there is some header include somewhere which is doing
-// the include as a side effect.
-typedef std::list<const Record * > cGroup_Value;
-
-#include "record.h"
-#include "engine.h"
 
 
 #endif /* DISAMBIGENGINE_H_ */
