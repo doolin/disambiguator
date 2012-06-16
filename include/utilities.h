6d0392630072a078fe80a6368875b741e7ecc344
Dave Doolin <david.doolin@gmail.com>
2012-06-16 12:33:08 -0700

    straightened up the header

--- include/utilities.h
+++ include/utilities.h
@@ -17,38 +17,38 @@ class cCluster_Set;
 class cRatios;
 
 
-bool   make_changable_training_sets_by_patent(const list <const Record*> & record_pointers,
-                                              const vector<string >& blocking_column_names,
-                                              const vector < const StringManipulator *> & pstring_oper,
-                                              const unsigned int limit,
-                                              const vector <string> & training_filenames);
+bool   make_changable_training_sets_by_patent   (const list <const Record*> & record_pointers,
+                                                 const vector<string >& blocking_column_names,
+                                                 const vector < const StringManipulator *> & pstring_oper,
+                                                 const unsigned int limit,
+                                                 const vector <string> & training_filenames);
 
-bool   make_stable_training_sets_by_personal (const list <Record> & all_records,
-                                              const unsigned int limit,
-                                              const vector <string> & training_filenames);
+bool   make_stable_training_sets_by_personal    (const list <Record> & all_records,
+                                                 const unsigned int limit,
+                                                 const vector <string> & training_filenames);
 
-bool   make_changable_training_sets_by_assignee(const list <const Record*> & record_pointers,
-                                                const vector<string >& blocking_column_names,
-                                                const vector < const StringManipulator *> & pstring_oper,
-                                                const unsigned int limit,
-                                                const vector <string> & training_filenames);
+bool   make_changable_training_sets_by_assignee (const list <const Record*> & record_pointers,
+                                                 const vector<string >& blocking_column_names,
+                                                 const vector < const StringManipulator *> & pstring_oper,
+                                                 const unsigned int limit,
+                                                 const vector <string> & training_filenames);
 
-int    unique_inventors_per_period (unsigned int starting_year,
-                                    unsigned int interval,
-                                    const char * wholedatabase,
-                                    const char * disambigresult,
-                                    const char * outputfile);
+int    unique_inventors_per_period              (unsigned int starting_year,
+                                                 unsigned int interval,
+                                                 const char * wholedatabase,
+                                                 const char * disambigresult,
+                                                 const char * outputfile);
 
-void   one_step_postprocess(const list < Record > & all_records,
-                            const char * last_disambig_result,
-                            const char * outputfile);
+void   one_step_postprocess                     (const list < Record > & all_records,
+                                                 const char * last_disambig_result,
+                                                 const char * outputfile);
 
-string remove_headtail_space(const string & s);
+string remove_headtail_space                    (const string & s);
 
-void   out_of_cluster_density(const cCluster_Set & upper,
-                              const cCluster_Set & lower,
-                              const cRatios & ratio,
-                              std::ofstream & ofile );
+void   out_of_cluster_density                   (const cCluster_Set & upper,
+                                                 const cCluster_Set & lower,
+                                                 const cRatios & ratio,
+                                                 std::ofstream & ofile );
 
 
 #endif /* PATENT_UTILITIES_H */
