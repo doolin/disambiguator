1db1c989d7e05e6c564c02dbb65af79c9ed1d3dd
Dave Doolin <david.doolin@gmail.com>
2012-06-27 18:00:38 -0700

    semantic improvements to the record tree creator

--- src/fileoper.cpp
+++ src/fileoper.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstring>
 
+
 #include "fileoper.h"
 #include "engine.h"
 #include "cluster.h"
@@ -21,25 +22,27 @@ retrieve_record_pointer_by_unique_id(const string & uid,
 
 void
 create_btree_uid2record_pointer(map<string, const Record *> & uid_tree,
-                                const list<Record> & reclist,
-                                const string& uid_name ) {
+                                const list<Record> & record_list,
+                                const string & uid_name ) {
+
 
     uid_tree.clear();
     const unsigned int uid_index = Record::get_index_by_name(uid_name);
     cException_Vector_Data except(uid_name.c_str());
 
     map <string, const Record *>::iterator pm;
+    list<Record>::const_iterator record;
+    for (record = record_list.begin(); record != record_list.end(); ++record ) {
 
-    for ( list<Record>::const_iterator p = reclist.begin(); p != reclist.end(); ++p ) {
-
-        const Attribute * pattrib = p->get_attrib_pointer_by_index(uid_index);
+        const Attribute * pattrib = record->get_attrib_pointer_by_index(uid_index);
         //if ( pattrib->get_data().size() != 1 ) throw except;
         const string & label = *pattrib->get_data().at(0);
         pm = uid_tree.find(label);
 
-        if ( pm != uid_tree.end())
+        if ( pm != uid_tree.end()) {
             throw cException_Duplicate_Attribute_In_Tree(label.c_str());
+	}
 
-        uid_tree.insert(std::pair< string, const Record *>(label, &(*p)));
+        uid_tree.insert(std::pair< string, const Record *>(label, &(*record)));
     }
 }
