62e9920b0ae7776a54894d921a0206c9959abf24
Dave Doolin <david.doolin@gmail.com>
2012-07-06 12:48:40 -0700

    started writing a unit test wrapper program for testing everything at once.

---
+++ test/unittest.h
@@ -0,0 +1,17 @@
+
+#include "test_typedefs.h"
+#include "test_comparators.h"
+#include "test_attribute.h"
+#include "test_blocking.h"
+
+
+void
+test_all() {
+
+}
+
+int
+main(int argc, char ** argv) {
+
+  return 0;
+}
