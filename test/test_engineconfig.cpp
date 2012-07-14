78c09ea4c23e93fbc68284841b15e15f12f425d0
Dave Doolin <david.doolin@gmail.com>
2012-07-14 13:14:43 -0700

    test fetching records

--- test/test_engineconfig.cpp
+++ test/test_engineconfig.cpp
@@ -5,21 +5,42 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <list>
 #include <string>
 #include <stdlib.h>
+
+#include "record.h"
+
+void
+printer(const std::vector<std::string> & columns) {
+
+  std::vector<std::string>::const_iterator citer = columns.begin();
+  for (; citer != columns.end(); ++citer) {
+    std::cout << (*citer).c_str() << std::endl;
+  }
+}
+
+std::vector<std::string>
+setup_columns() {
+
+  std::vector<std::string> involved_columns;
+  involved_columns.push_back (std::string("Lastname"));
+  involved_columns.push_back (std::string("Firstname"));
+  involved_columns.push_back (std::string("Middlename"));
+  printer(involved_columns);
+  return involved_columns;
+}
+
 
 
 int
 main(int argc, char ** argv) {
 
- /* This is what the main does for handling the engine and
-  * blocking configuration files. We'll mirror it for now.
-  * TODO: Simplify the file handling for engine and blocking.
-  */
-  std::string engineconf("./testdata/engine.txt");
-  const char * EngineConfigFile = engineconf.c_str();
-  EngineConfiguration::config_engine(EngineConfigFile, std::cout)
+  std::vector<std::string> involved_columns = setup_columns();
+  char filename[] = "./testdata/invpat_onerecord.txt";
+  std::list <Record> all_records;
+  fetch_records_from_txt(all_records, filename, involved_columns);
 
   return 0;
-
 }
