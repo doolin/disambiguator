0482cfa477605f43f213781c13815535edd36d9e
Dave Doolin <david.doolin@gmail.com>
2012-07-18 22:13:42 -0700

    white space cleanup

--- src/strcmp95.c
+++ src/strcmp95.c
@@ -269,8 +269,10 @@ int is_misspell( const char * s1, const char * s2 ) {
 
 }
 
-int is_abbreviation( const char * s1, const char * s2 ) {
-        int cnt = 0;
+int
+is_abbreviation(const char * s1, const char * s2) {
+
+    int cnt = 0;
     while ( *s1 != '\0' && *s2 != '\0' && *s1 == *s2 ) {
         ++s1;
         ++s2;
@@ -283,29 +285,3 @@ int is_abbreviation( const char * s1, const char * s2 ) {
     else
         return cnt;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
