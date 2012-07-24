e45f9045842834be08c6c4072b1f3247e61ceab1
Dave Doolin <david.doolin@gmail.com>
2012-07-24 14:12:44 -0700

    finished abbreviation unit test

--- test/test_abbreviation.cpp
+++ test/test_abbreviation.cpp
@@ -10,7 +10,7 @@
 #include "strcmp95.h"
 
 /**
- * Run this code with /usr/bin/valgrind ./abbreviations --leak-check=full
+ * Run this code with /usr/bin/valgrind ./abbreviation --leak-check=full
  */
 
 void
@@ -18,12 +18,22 @@ test_is_abbrev() {
 
   const char * s1 = "abbreviation";
   const char * s2 = "abbr";
-  CPPUNIT_ASSERT (3 == is_abbreviation(s1,s2));
+  CPPUNIT_ASSERT (4 == is_abbreviation(s1,s2));
 }
 
+/**
+ * This tests the implementation, which passes, with
+ * the caveat that it doesn't implement an algorithm
+ * conforming to any standard, accepted definition of
+ * an abbreviation. This might could be called something
+ * else, like "custom_stemming" or something.
+ */
 void
 test_is_not_abbrev() {
 
+  const char * s1 = "abbreviation";
+  const char * s2 = "abbr.";
+  CPPUNIT_ASSERT (0 == is_abbreviation(s1,s2));
 }
 
 void
