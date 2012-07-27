f78ef81998e080a77a239c398f649a6691531c9d
Dave Doolin <david.doolin@gmail.com>
2012-07-26 20:58:44 -0700

    CPLEX now defined, reordered header includes appropriately

--- src/ratio_smoothing.cpp
+++ src/ratio_smoothing.cpp
@@ -5,20 +5,22 @@
  *      Author: ysun
  */
 
-#include "ratios.h"
-#include "engine.h"
-
 #include <limits>
 #include <climits>
 #include <sstream>
 #include <cmath>
 #include <algorithm>
 
-#define HAVE_CPLEX 1
+#include <string.h>
 
+#define HAVE_CPLEX 1
 #if HAVE_CPLEX
 #include <ilcplex/ilocplex.h>
 #endif
+
+#include "ratios.h"
+#include "engine.h"
+
 
 static const bool should_do_name_range_check = true;
 
