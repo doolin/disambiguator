0dc6c9a012a75c829d60357e3d12e0fa1bde2c8d
Dave Doolin <david.doolin@gmail.com>
2012-08-21 15:13:26 -0700

    factored out clusterhead into its own header file.

--- include/newcluster.h
+++ include/newcluster.h
@@ -3,39 +3,7 @@
 #define PATENT_NEWCLUSTER_H
 
 #include "engine.h"
-
-/**
- * ClusterHead:
- * This class contains two pieces of information about
- * a cluster: its delegate and its cohesion.
- */
-class ClusterHead {
-
-public:
-
- /** const Record * m_delegate: the delegate (representative) of a cluster.
-  * Usually this pointer contains the most frequently occurring information.
-  */
-  const Record * m_delegate;
-
- /**
-  *  double m_cohesion: the cohesion of a cluster, meaning the probability
-  *  for the members of the cluster to be of the same inventor.
-  */
-  double m_cohesion;
-  
- /**
-  *  ClusterHead(const Record * const p, const double c): constructor
-  */
-  ClusterHead(const Record * const p, const double c): m_delegate(p), m_cohesion(c) {};
-
- /**
-  * ClusterHead ( const ClusterHead & rhs): copy constructor
-  */
-  ClusterHead ( const ClusterHead & rhs): m_delegate(rhs.m_delegate), m_cohesion(rhs.m_cohesion) {}
-};
-
-
+#include "clusterhead.h"
 
 /**
  * Cluster objects are the molecules of disambiguation,
