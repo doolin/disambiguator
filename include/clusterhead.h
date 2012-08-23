416f27b66b6a133a4246461cb7b0ffa32a49c865
Dave Doolin <david.doolin@gmail.com>
2012-08-22 20:55:30 -0700

    spiffed up clusterhead a little bit

--- include/clusterhead.h
+++ include/clusterhead.h
@@ -20,16 +20,18 @@ public:
   *  for the members of the cluster to be of the same inventor.
   */
   double m_cohesion;
-  
+
  /**
   *  ClusterHead(const Record * const p, const double c): constructor
   */
-  ClusterHead(const Record * const p, const double c): m_delegate(p), m_cohesion(c) {};
+  ClusterHead(const Record * const representative, const double cohesion)
+              : m_delegate(representative), m_cohesion(cohesion) {};
 
  /**
   * ClusterHead ( const ClusterHead & rhs): copy constructor
   */
-  ClusterHead ( const ClusterHead & rhs): m_delegate(rhs.m_delegate), m_cohesion(rhs.m_cohesion) {}
+  ClusterHead (const ClusterHead & rhs)
+               : m_delegate(rhs.m_delegate), m_cohesion(rhs.m_cohesion) {}
 };
 
 #endif /* PATENT_CLUSTERHEAD_H */
