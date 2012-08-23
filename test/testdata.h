f1b5150bd91851c7c395a75181702d15cccfe39c
Dave Doolin <david.doolin@gmail.com>
2012-08-23 12:53:52 -0700

    attempting to ramp up cluster unit testing

--- test/testdata.h
+++ test/testdata.h
@@ -2,9 +2,11 @@
 #ifndef PATENT_TEST_TESTDATA_H
 #define PATENT_TEST_TESTDATA_H
 
+#include <engine.h> // for RecordList
 #include <record.h>
 
-Record *  make_foobar_record(void);
-Record *  make_quuxalot_record(void);
+Record     * make_foobar_record(void);
+Record     * make_quuxalot_record(void);
+RecordList   get_record_list(void);
 
 #endif // PATENT_TEST_TESTDATA_H
