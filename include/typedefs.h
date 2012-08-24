739609e1d608cec4555ea947bee4d7ba923f7251
Dave Doolin <david.doolin@gmail.com>
2012-08-24 07:59:30 -0700

    can only typedef containers from primitives at the moment

--- include/typedefs.h
+++ include/typedefs.h
@@ -5,15 +5,17 @@
 #include <vector>
 #include <string>
 
+
 typedef std::vector<std::string> Label;
 
 // SimilarityProfile will probably get moved back
 // to the ratios and smoothing code once that us
 // cleaned up and refactored more.
-typedef std::vector < unsigned int > SimilarityProfile;
+typedef std::vector<unsigned int> SimilarityProfile;
 
 //asgdetail consists of assignee number and its patent counts.
 typedef std::pair<std::string, unsigned int> asgdetail;
 
+//typedef map<const Record *, RecordList, cSort_by_attrib> PatentTree;
 
 #endif /* PATENT_TYPEDEFS_H */
