24446d92d26dca6f7a87814709bc94d79816ddec
Dave Doolin <david.doolin@gmail.com>
2012-08-25 12:49:38 -0700

    removed tabs, cleanup

--- include/disambiguate.h
+++ include/disambiguate.h
@@ -1,40 +1,35 @@
-/*
- * Disambigmain.h
- *
- *  Created on: Jan 10, 2011
- *      Author: ysun
- */
+/** @file */
 
-#ifndef DISAMBIGMAIN_H_
-#define DISAMBIGMAIN_H_
+#ifndef PATENT_DISAMBIGMAIN_H
+#define PATENT_DISAMBIGMAIN_H
 
 namespace EngineConfiguration {
-	bool config_engine( const char * filename, std::ostream & os );
-
+    bool config_engine( const char * filename, std::ostream & os );
 }
+
 namespace BlockingConfiguration {
-	class cBlockingDetail {
-	public:
-		StringManipulator * m_psm;
-		std::string m_columnname;
-		unsigned int m_dataindex;
-		int m_begin;
-		unsigned int m_nchar;
-		bool m_isforward;
-		cBlockingDetail()  {
-			m_psm  = new StringNoSpaceTruncate ;
-		}
-		cBlockingDetail ( const cBlockingDetail & rhs ) {
-			m_psm = rhs.m_psm->clone();
-			m_columnname = rhs.m_columnname;
-			m_dataindex = rhs.m_dataindex;
-			m_begin = rhs.m_begin;
-			m_nchar = rhs.m_nchar;
-			m_isforward = rhs.m_isforward;
-		}
-		~cBlockingDetail() { delete m_psm; }
-	};
-	int config_blocking ( const char * filename, const string & module_id);
+    class cBlockingDetail {
+    public:
+        StringManipulator * m_psm;
+        std::string m_columnname;
+        unsigned int m_dataindex;
+        int m_begin;
+        unsigned int m_nchar;
+        bool m_isforward;
+        cBlockingDetail()  {
+            m_psm  = new StringNoSpaceTruncate ;
+        }
+        cBlockingDetail ( const cBlockingDetail & rhs ) {
+            m_psm = rhs.m_psm->clone();
+            m_columnname = rhs.m_columnname;
+            m_dataindex = rhs.m_dataindex;
+            m_begin = rhs.m_begin;
+            m_nchar = rhs.m_nchar;
+            m_isforward = rhs.m_isforward;
+        }
+        ~cBlockingDetail() { delete m_psm; }
+    };
+    int config_blocking ( const char * filename, const string & module_id);
 }
 
 int Full_Disambiguation( const char * EngineConfigFile, const char * BlockingConfigFile );
@@ -42,4 +37,4 @@ int Full_Disambiguation( const char * EngineConfigFile, const char * BlockingCon
 
 
 
-#endif /* DISAMBIGMAIN_H_ */
+#endif /* PATENT_DISAMBIGMAIN_H */
