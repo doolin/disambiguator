81b8d842e312fa335fb3724359da6b21e179855b
Dave Doolin <david.doolin@gmail.com>
2012-08-24 17:26:07 -0700

    diff final.txt now works, next is to abstract the function running the disambiguation

--- test/integration.sh
+++ test/integration.sh
@@ -3,50 +3,47 @@
 function test_jones() {
 
   current_dir=`pwd`
-  #echo $current_dir
+  dir=/var/share/patentdata/disambiguation/test/synthetic/jones
+  cd $dir
 
-  jones_dir=/var/share/patentdata/disambiguation/test/synthetic/jones
-  cd $jones_dir
-  #echo $jones_dir
   disambiguate engine.txt blocking6.txt > runtime.log
 
   cd $current_dir
   #echo `pwd`
-  testfile=$jones_dir"/final.txt"
-  #echo $testfile
-  #cat $testfile
-  #echo "0"
+  output=$dir"/final.txt"
+  testfile="./data/final_jones.txt"
+  results=`diff ${output} ${testfile}`
+  echo "jones diff resulted in $results"
 }
 
 function test_eighteen() {
 
   current_dir=`pwd`
-  #echo $current_dir
+  dir=/var/share/patentdata/disambiguation/test/18
+  cd $dir
 
-  jones_dir=/var/share/patentdata/disambiguation/test/18
-  cd $jones_dir
-  #echo $jones_dir
   disambiguate engine.txt blocking6.txt > runtime.log
 
   cd $current_dir
-  #echo `pwd`
-  testfile=$jones_dir"/final.txt"
-  #echo $testfile
-  cat $testfile
-  # Do the diff here, capture the result to echo back
-  #echo "0"
+  output=$dir"/final.txt"
+  testfile="./data/final_eighteen.txt"
+  results=`diff ${output} ${testfile}`
+  echo "eighteen diff resulted in $results"
 }
 
+source ./bashcolors.sh
+initializeANSI
 
 
 testval=`test_jones`
-#echo "Jones result:  $testval"
+echo $cyanf"Jones result:  $testval"
 
 testval=`test_eighteen`
-#echo "Eighteent:  $testval"
+echo $orangeyf"Eighteen: $testval"
 
 #echo "arg 0:  $0"
 #echo "arg 1:  $1"
 #echo "arg 2:  $2"
 #echo "arg 3:  $3"
 
+echo $reset
