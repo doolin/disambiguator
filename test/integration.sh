83e0c094a725a6b178ee2078fcb260062a0b1d6b
Dave Doolin <david.doolin@gmail.com>
2012-08-25 11:36:17 -0700

    removed superfluous functions

--- test/integration.sh
+++ test/integration.sh
@@ -3,44 +3,6 @@
 source ./bashcolors.sh
 initializeANSI
 
-function test_jones() {
-
-  current_dir=`pwd`
-  dir=/var/share/patentdata/disambiguation/test/synthetic/jones
-  cd $dir
-
-  disambiguate engine.txt blocking6.txt > runtime.log
-
-  cd $current_dir
-  #echo `pwd`
-  output=$dir"/final.txt"
-  testfile="./data/final_jones.txt"
-  results=`diff ${output} ${testfile}`
-  echo "diff resulted in $results"
-}
-
-function test_eighteen() {
-
-  current_dir=`pwd`
-  dir=/var/share/patentdata/disambiguation/test/18
-  cd $dir
-
-  disambiguate engine.txt blocking6.txt > runtime.log
-
-  cd $current_dir
-  output=$dir"/final.txt"
-  testfile="./data/final_eighteen.txt"
-  results=`diff ${output} ${testfile}`
-  #echo "diff resulted in $results"
-  echo ${#results}
-  if [[ $results ]] ; then
-    echo "diff resulted in $results"
-    echo $redf"failed"
-  else
-    echo $greenf"passed"
-  fi
-}
-
 
 function test_disambiguation() {
 
@@ -62,7 +24,8 @@ function test_disambiguation() {
   fi
 }
 
-
+# TODO: Create an array for the test directories and files,
+# then loop it to run the tests.
 testdir=/var/share/patentdata/disambiguation/test/synthetic/jones
 testfile="./data/final_jones.txt"
 testval=`test_disambiguation $testdir $testfile`
