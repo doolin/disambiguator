df4dfa2c0ed40716696355a27d0a09a80bf41fcc
Dave Doolin <david.doolin@gmail.com>
2012-08-25 20:13:05 -0700

    integration script now working with berkeley rare names

--- test/integration.sh
+++ test/integration.sh
@@ -3,6 +3,9 @@
 source ./bashcolors.sh
 initializeANSI
 
+echo
+echo $cyanf"# Integration testing for disambiguator"
+echo
 
 function test_disambiguation() {
 
@@ -16,35 +19,52 @@ function test_disambiguation() {
   output=$dir"/final.txt"
   testfile=$2
   results=`diff ${output} ${testfile}`
-  #echo ${#results}
   if [[ $results ]] ; then
-    echo $redf"failed: diff results in $results"
+    echo $redf"failed final: diff results in $results"
   else
-    echo $greenf"passed"
+    echo $greenf"passed final"
+  fi
+
+
+
+  if [[ $3 ]] ; then
+    echo "\n"
+    rare=$dir"/Rare_Names.txt"
+    results=`diff ${rare} $3`
+    if [[ $results ]] ; then
+      echo $redf"failed rare names: diff results in $results"
+    else
+      echo $greenf"passed rare names"
+    fi
   fi
 }
+
 
 # TODO: Create an array for the test directories and files,
 # then loop it to run the tests.
 testdir=/var/share/patentdata/disambiguation/test/synthetic/jones
 testfile="./data/final_jones.txt"
 testval=`test_disambiguation $testdir $testfile`
-echo $cyanf"Jones:  $testval"
+echo $cyanf"## Jones"
+echo $testval
+
+echo
 
 testdir=/var/share/patentdata/disambiguation/test/18
 testfile="./data/final_eighteen.txt"
 testval=`test_disambiguation $testdir $testfile`
-echo $cyanf"Eighteen: $testval"
+echo $cyanf"## Eighteen"
+echo $testval
+
+echo
 
 testdir=/var/share/patentdata/disambiguation/experiments/earth/berkeley
-testfile="./data/final_berkeley.txt"
-testval=`test_disambiguation $testdir $testfile`
-echo $cyanf"Berkeley: $testval"
+finalfile="./data/final_berkeley.txt"
+rarenamefile="./data/rare_names_berkeley.txt"
+testval=`test_disambiguation $testdir $finalfile $rarenamefile`
+echo $cyanf"## Berkeley"
+echo -e $testval
 
-
-#echo "arg 0:  $0"
-#echo "arg 1:  $1"
-#echo "arg 2:  $2"
-#echo "arg 3:  $3"
 
 echo $reset
+
