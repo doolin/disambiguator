a743441c33085c10c1a0117be9982aacda6f61f5
Dave Doolin <david.doolin@gmail.com>
2012-09-01 20:39:34 -0700

    general cleanup in blocking oepration

--- src/blocking_operation.cpp
+++ src/blocking_operation.cpp
@@ -18,6 +18,7 @@ cBlocking_Operation_Multiple_Column_Manipulate::cBlocking_Operation_Multiple_Col
 
     if ( inputvsm.size() != columnnames.size() )
         throw cException_Other("Critical Error in cBlocking_Operation_Multiple_Column_Manipulate: size of string manipulaters is different from size of columns");
+
     for ( unsigned int i = 0; i < columnnames.size(); ++i ) {
         indice.push_back(Record::get_index_by_name( columnnames.at(i)));
         infoless += delim;
@@ -26,9 +27,11 @@ cBlocking_Operation_Multiple_Column_Manipulate::cBlocking_Operation_Multiple_Col
 }
 
 
-cBlocking_Operation_Multiple_Column_Manipulate::cBlocking_Operation_Multiple_Column_Manipulate (const StringManipulator * const* pinputvsm, const string * pcolumnnames, const unsigned int  * pdi, const unsigned int num_col ) {
+cBlocking_Operation_Multiple_Column_Manipulate::cBlocking_Operation_Multiple_Column_Manipulate (
+    const StringManipulator * const * pinputvsm, const string * pcolumnnames,
+    const unsigned int  * pdi, const unsigned int num_col) {
 
-    for ( unsigned int i = 0; i < num_col; ++i ) {
+    for (unsigned int i = 0; i < num_col; ++i) {
         vsm.push_back(*pinputvsm++);
         attributes_names.push_back(*pcolumnnames);
         indice.push_back(Record::get_index_by_name(*pcolumnnames++));
@@ -50,8 +53,8 @@ string
 cBlocking_Operation_Multiple_Column_Manipulate::extract_blocking_info(const Record * p) const {
 
     string temp;
-    for ( unsigned int i = 0; i < vsm.size(); ++i ) {
-        temp += vsm[i]->manipulate(* p->get_data_by_index(indice[i]).at(  pdata_indice.at(i)));
+    for (unsigned int i = 0; i < vsm.size(); ++i) {
+        temp += vsm[i]->manipulate(* p->get_data_by_index(indice[i]).at( pdata_indice.at(i)));
         temp += delim;
     }
     return temp;
@@ -59,7 +62,7 @@ cBlocking_Operation_Multiple_Column_Manipulate::extract_blocking_info(const Reco
 
 
 void
-cBlocking_Operation_Multiple_Column_Manipulate::reset_data_indice ( const vector < unsigned int > & indice ) {
+cBlocking_Operation_Multiple_Column_Manipulate::reset_data_indice (const vector < unsigned int > & indice ) {
 
     if ( indice.size() != this->pdata_indice.size() )
         throw cException_Other("Indice size mismatch. cannot reset.");
@@ -71,10 +74,10 @@ cBlocking_Operation_Multiple_Column_Manipulate::reset_data_indice ( const vector
 /**
  * Aim: to extract a specific blocking string. look at the header file for mor details.
  */
-string cBlocking_Operation_Multiple_Column_Manipulate::extract_column_info ( const Record * p, unsigned int flag ) const {
+string cBlocking_Operation_Multiple_Column_Manipulate::extract_column_info (const Record * p, unsigned int flag) const {
     if ( flag >= indice.size() )
         throw cException_Other("Flag index error.");
-    return vsm[flag]->manipulate( * p->get_data_by_index(indice[flag]).at( pdata_indice.at(flag)) );
+    return vsm[flag]->manipulate( * p->get_data_by_index(indice[flag]).at( pdata_indice.at(flag)));
 }
 
 
@@ -87,7 +90,7 @@ string cBlocking_Operation_Multiple_Column_Manipulate::extract_column_info ( con
  * of the building of the uid2uinv tree.
  */
 cBlocking_Operation_By_Coauthors::cBlocking_Operation_By_Coauthors(
-    const list < const Record * > & all_rec_pointers,
+    const RecordPList & all_rec_pointers,
     const ClusterInfo & cluster, const unsigned int coauthors)
     : patent_tree(cSort_by_attrib(cPatent::static_get_class_name())), num_coauthors(coauthors) {
 
@@ -108,7 +111,8 @@ cBlocking_Operation_By_Coauthors::cBlocking_Operation_By_Coauthors(
 
 
 
-cBlocking_Operation_By_Coauthors::cBlocking_Operation_By_Coauthors(const list < const Record * > & all_rec_pointers, const unsigned int coauthors)
+cBlocking_Operation_By_Coauthors::cBlocking_Operation_By_Coauthors(
+    const RecordPList & all_rec_pointers, const unsigned int coauthors)
     : patent_tree(cSort_by_attrib(cPatent::static_get_class_name())), num_coauthors(coauthors) {
 
     if ( num_coauthors > 4 ) {
@@ -116,7 +120,9 @@ cBlocking_Operation_By_Coauthors::cBlocking_Operation_By_Coauthors(const list < 
         std::cout << "Number of coauthors in which cBlocking_Operation_By_Coauthors uses is probably too large. Number of coauthors = " << num_coauthors << std::endl;
         std::cout << "==================END OF WARNING ================" << std::endl;
     }
+
     build_patent_tree(all_rec_pointers);
+
     for ( unsigned int i = 0; i < num_coauthors; ++i ) {
         infoless += cBlocking_Operation::delim + cBlocking_Operation::delim;
         infoless += cBlocking_Operation::delim + cBlocking_Operation::delim;
@@ -137,10 +143,13 @@ cBlocking_Operation_By_Coauthors::cBlocking_Operation_By_Coauthors(const list < 
  * and append p into the list.
  */
 void
-cBlocking_Operation_By_Coauthors::build_patent_tree(const list < const Record * > & all_rec_pointers) {
+cBlocking_Operation_By_Coauthors::build_patent_tree(
+    const RecordPList & all_rec_pointers) {
 
     map < const Record *, RecordPList, cSort_by_attrib >::iterator ppatentmap;
-    for ( list < const Record * >::const_iterator p = all_rec_pointers.begin(); p != all_rec_pointers.end(); ++p ) {
+
+    RecordPList::const_iterator p = all_rec_pointers.begin();
+    for (; p != all_rec_pointers.end(); ++p ) {
         ppatentmap = patent_tree.find(*p);
         if ( ppatentmap == patent_tree.end() ) {
             RecordPList temp ( 1, *p);
@@ -154,9 +163,12 @@ cBlocking_Operation_By_Coauthors::build_patent_tree(const list < const Record * 
 
 
 /*
- * Aim: to create binary tree of unique record id -> unique inventor id, also to allow fast search and insertion/deletion.
- *         the unique inventor id is also a const Record pointer, meaning that different unique record ids may be associated with a same
- *         const Record pointer that represents them.
+ * Aim: to create binary tree of unique record id -> unique inventor id,
+ * also to allow fast search and insertion/deletion.
+ * the unique inventor id is also a const Record pointer,
+ * meaning that different unique record ids may be associated with a same
+ * const Record pointer that represents them.
+ *
  * Algorithm: clean the uinv2count and uid2uinv tree first.
  *                 For any cluster in the cCluser_Info object:
  *                     For any const Record pointer p in the cluster member list:
@@ -173,10 +185,15 @@ cBlocking_Operation_By_Coauthors::build_uid2uinv_tree(const ClusterInfo & cluste
     uid2uinv_tree.clear();
     unsigned int count = 0;
     typedef list<Cluster> cRecGroup;
+
     std::cout << "Building trees: 1. Unique Record ID to Unique Inventer ID. 2 Unique Inventer ID to Number of holding patents ........" << std::endl;
-    //for ( map < string, cRecGroup >::const_iterator p = cluster.get_cluster_map().begin(); p != cluster.get_cluster_map().end(); ++p ) {
-    for ( map < string, cRecGroup >::const_iterator p = cluster.get_cluster_map().begin(); p != cluster.get_cluster_map().end(); ++p ) {
-        for ( cRecGroup::const_iterator q = p->second.begin(); q != p->second.end(); ++q ) {
+
+    map < string, cRecGroup >::const_iterator p = cluster.get_cluster_map().begin();
+    for (; p != cluster.get_cluster_map().end(); ++p) {
+
+        cRecGroup::const_iterator q = p->second.begin();
+        for (;  q != p->second.end(); ++q) {
+
             const Record * value = q->get_cluster_head().m_delegate;
             map < const Record *, unsigned int >::iterator pcount = uinv2count_tree.find(value);
             if ( pcount == uinv2count_tree.end() )
@@ -195,9 +212,15 @@ cBlocking_Operation_By_Coauthors::build_uid2uinv_tree(const ClusterInfo & cluste
 
 
 /*
- * Aim: to get a list of top N coauthors ( as represented by a const Record pointer ) of an inventor to whom prec ( a unique record id ) belongs.
+ * Aim: to get a list of top N coauthors (as represented by a const Record pointer)
+ * of an inventor to whom prec (a unique record id) belongs.
+ *
  * Algorithm:
- *         1. create a binary tree T( std::map ). Key = number of holding patents, value = const Record pointer to the unique inventor.
+ *
+ *         1. create a binary tree T(std::map)
+ *            Key = number of holding patents,
+ *            value = const Record pointer to the unique inventor.
+ *
  *         2. For any associated record r:
  *                 find r in the uinv2count tree.
  *                 if number of nodes in T < N, insert (count(r), r) into T;
@@ -205,55 +228,66 @@ cBlocking_Operation_By_Coauthors::build_uid2uinv_tree(const ClusterInfo & cluste
  *                     if count(r) > front of T:
  *                         delete front of T from T
  *                         insert ( count(r), r );
+ *
  *         3. return values in T.
  *
  */
-RecordPList cBlocking_Operation_By_Coauthors::get_topN_coauthors(const Record * prec,
-                                                                  const unsigned int topN ) const {
+RecordPList cBlocking_Operation_By_Coauthors::get_topN_coauthors(
+    const Record * prec, const unsigned int topN ) const {
 
     const RecordPList & list_alias = patent_tree.find(prec)->second;
     map < unsigned int, RecordPList > occurrence_map;
     unsigned int cnt = 0;
 
-    for (RecordPList::const_iterator p = list_alias.begin(); p != list_alias.end(); ++p) {
+    RecordPList::const_iterator p = list_alias.begin();
+    for (; p != list_alias.end(); ++p) {
 
-        if ( *p == prec )
-            continue;
+        if ( *p == prec ) continue;
 
         map < const Record *, const Record * >::const_iterator puid2uiv = uid2uinv_tree.find(*p);
         if ( puid2uiv == uid2uinv_tree.end() )
             throw cException_Other("Critical Error: unique record id to unique inventer id tree is incomplete!!");
+
         const Record * coauthor_pointer = puid2uiv->second;
 
         map < const Record *, unsigned int >::const_iterator puinv2count = uinv2count_tree.find(coauthor_pointer);
+
         if ( puinv2count == uinv2count_tree.end())
             throw cException_Other("Critical Error: unique inventer id to number of holding patents tree is incomplete!!");
+
         const unsigned int coauthor_count = puinv2count->second;
 
-        if ( cnt <= topN || coauthor_count > occurrence_map.begin() ->first ) {
-            map < unsigned int, RecordPList >::iterator poccur = occurrence_map.find ( coauthor_count );
-            if ( poccur == occurrence_map.end() ) {
+        if (cnt <= topN || coauthor_count > occurrence_map.begin() ->first) {
+
+            map < unsigned int, RecordPList >::iterator poccur = occurrence_map.find (coauthor_count);
+            if (poccur == occurrence_map.end()) {
                 RecordPList temp (1, coauthor_pointer);
                 occurrence_map.insert(std::pair<unsigned int, RecordPList>(coauthor_count, temp));
             }
-            else
+            else {
                 poccur->second.push_back(coauthor_pointer);
+            }
 
             if ( cnt < topN )
                 ++cnt;
             else {
                 map < unsigned int, RecordPList >::iterator pbegin = occurrence_map.begin();
                 pbegin->second.pop_back();
-                if ( pbegin->second.empty() )
+                if (pbegin->second.empty()) {
                     occurrence_map.erase(pbegin);
+                }
             }
         }
     }
 
     //output
     RecordPList ans;
-    for ( map < unsigned int, RecordPList>::const_reverse_iterator rp = occurrence_map.rbegin(); rp != occurrence_map.rend(); ++rp )
+
+    map < unsigned int, RecordPList>::const_reverse_iterator rp = occurrence_map.rbegin();
+    for (; rp != occurrence_map.rend(); ++rp ) {
         ans.insert(ans.end(), rp->second.begin(), rp->second.end());
+    }
+
     return ans;
 }
 
@@ -270,14 +304,15 @@ string cBlocking_Operation_By_Coauthors::extract_blocking_info(const Record * pr
     const unsigned int lastnameindex = Record::get_index_by_name(cLastname::static_get_class_name());
 
     string answer;
-    for ( RecordPList::const_iterator p = top_coauthor_list.begin(); p != top_coauthor_list.end(); ++p ) {
+
+    RecordPList::const_iterator p = top_coauthor_list.begin();
+    for (; p != top_coauthor_list.end(); ++p) {
         answer += *(*p)->get_data_by_index(firstnameindex).at(0);
         answer += cBlocking_Operation::delim;
         answer += *(*p)->get_data_by_index(lastnameindex).at(0);
         answer += cBlocking_Operation::delim;
     }
-    if ( answer.empty() )
-        answer = infoless;
+    if (answer.empty()) answer = infoless;
     return answer;
 }
 
