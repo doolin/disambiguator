070c744fd56e47180ef67001008281387c779011
Dave Doolin <david.doolin@gmail.com>
2012-09-02 13:44:54 -0700

    changed record header to use uint32_t

--- include/record.h
+++ include/record.h
@@ -10,6 +10,9 @@
 #include <set>
 #include <algorithm>
 
+// TODO: Move this into a header all other headers include:
+#include <stdint.h>
+
 #include "attribute.h"
 #include "threading.h"
 
@@ -85,38 +88,37 @@ public:
     }
 
    /**
-    *  vector <unsigned int> record_compare(const Record & rhs) const:
+    *  vector <uint32_t> record_compare(const Record & rhs) const:
     *  compare (*this) with rhs and get a similarity profile.
-    *  NOTE THAT SIMILARITY PROFILES ARE vector <unsigned int>s;
+    *  NOTE THAT SIMILARITY PROFILES ARE vector <uint32_t>s;
     *  Example: if (Firstname, Lastname, Assignee, class, Coauthor) are
     *  activated for comparison, the function will return a
-    *  vector < unsigned int > of 5-dimensions, each of which
+    *  vector < uint32_t > of 5-dimensions, each of which
     *  indicating a score for its corresponding column.
     */
-    vector <unsigned int> record_compare(const Record & rhs) const;
+    vector <uint32_t> record_compare(const Record & rhs) const;
 
    /**
-    *  vector <unsigned int> record_compare_by_attrib_indice (const Record &rhs,
-    *  const vector < unsigned int > & attrib_indice_to_compare) const:
+    *  vector <uint32_t> record_compare_by_attrib_indice (const Record &rhs,
+    *  const vector < uint32_t > & attrib_indice_to_compare) const:
     *
     *  compare (*this) with rhs only in the columns whose indice are given
     *  in "attrib_indice_to_compare", and returns an incomplete similarity profile.
     *  Example: if (Firstname, Lastname, Assignee, class, Coauthor) are activated,
     *  and the attrib_indice_to_compare = [ 0, 2, 3],
-    *  then the return value is a vector < unsigned int > = [ Firstname, Assignee, Class];
+    *  then the return value is a vector < uint32_t > = [ Firstname, Assignee, Class];
     */
-    vector <unsigned int> record_compare_by_attrib_indice (const Record &rhs,
-        const vector < unsigned int > & attrib_indice_to_compare) const;
+    vector <uint32_t> record_compare_by_attrib_indice (const Record &rhs,
+        const vector < uint32_t > & attrib_indice_to_compare) const;
 
 
 /**
- * static unsigned int get_index_by_name(const string & inputstr):
+ * static uint32_t get_index_by_name(const string & inputstr):
  * get the index of the attribute whose specifier is "inputstr"
- *
- * const vector <const string * > & get_data_by_index(const unsigned int i) const:
- * get the REAL data of the ith attribute through its index.
- *
- * const Attribute * const & get_attrib_pointer_by_index(const unsigned int i) const:
+ */
+
+/**
+ * const Attribute * const & get_attrib_pointer_by_index(const uint32_t i) const:
  *  get the reference of the const pointer to a const attribute based on its index.
  *  Returning of reference is critical,
  *  because this allows modification of the attribute pointer in the vector.
@@ -133,31 +135,45 @@ public:
  * static const vector < string > & get_column_names():
  * get the attribute names vector.
  *
- * unsigned int informative_attributes() const:
+ * uint32_t informative_attributes() const:
  * count the number of informative attributes (those containing
  * "something" rather than empty strings).
  */
 
-    static unsigned int get_index_by_name(const string & inputstr);
-    const vector <const string * > & get_data_by_index(const unsigned int i) const {return vector_pdata.at(i)->get_data();};
+    static uint32_t get_index_by_name(const string & inputstr);
+
+   /**
+    * const vector <const string * > & get_data_by_index(const uint32_t i) const:
+    * get the REAL data of the ith attribute through its index.
+    *
+    * (As opposed to the "FAKE" data...?)
+    */
+    const vector <const string *> & get_data_by_index(const uint32_t i) const {
+      // vector_pdata is vector<Attribute *>
+      // The get_data() function returns, as noted in this function's definition,
+      // a vector<const string *>, but I'm not sure what these strings are.
+      // Probably, they are simply the attribute names. It's all so horribly meta.
+      // TODO: try printing this out in the record unit test.
+      return vector_pdata.at(i)->get_data();
+    };
 
     //return a reference is very important, because the content can be changed.
-    const Attribute * const & get_attrib_pointer_by_index(const unsigned int i) const {return vector_pdata.at(i);}
+    const Attribute * const & get_attrib_pointer_by_index(const uint32_t i) const {return vector_pdata.at(i);}
     const vector < const Attribute*> & get_attrib_vector () const { return vector_pdata;}
     void print( std::ostream & os ) const;
 
     void print() const; //{ this->print(std::cout);}
     static const vector < string > & get_column_names() { return column_names;}
-    unsigned int informative_attributes() const;
+    uint32_t informative_attributes() const;
 
 
 /**
- * unsigned int record_exact_compare(const Record & rhs ) const:
+ * uint32_t record_exact_compare(const Record & rhs ) const:
  * compare (*this) with rhs and return the number of attributes that are exactly the same.
  */
 
 /**
- * void set_attrib_pointer_by_index( const Attribute * pa, const unsigned int i ):
+ * void set_attrib_pointer_by_index( const Attribute * pa, const uint32_t i ):
  * modify the attribute vector, setting the ith element to pa
  */
 
@@ -168,7 +184,7 @@ public:
  */
 
 /**
- * static unsigned int record_size():
+ * static uint32_t record_size():
  * returns the number of attributes of a record.
  */
 
@@ -185,24 +201,24 @@ public:
 
 
 /**
- * static unsigned int get_similarity_index_by_name(const string & inputstr):
+ * static uint32_t get_similarity_index_by_name(const string & inputstr):
  * get the index of an attribute in the ACTIVE similarity profile.
  */
 
 
-    unsigned int record_exact_compare(const Record & rhs ) const;
+    uint32_t record_exact_compare(const Record & rhs ) const;
 
-    void set_attrib_pointer_by_index( const Attribute * pa, const unsigned int i ) { vector_pdata.at(i) = pa;}
+    void set_attrib_pointer_by_index( const Attribute * pa, const uint32_t i ) { vector_pdata.at(i) = pa;}
 
     static void clean_member_attrib_pool();
 
-    static unsigned int record_size() { return column_names.size();}
+    static uint32_t record_size() { return column_names.size();}
 
     static void update_active_similarity_names();
 
     static const vector < string > & get_similarity_names() { return active_similarity_names;}
 
-    static unsigned int get_similarity_index_by_name(const string & inputstr);
+    static uint32_t get_similarity_index_by_name(const string & inputstr);
 
 /**
  * static const Record & get_sample_record():
