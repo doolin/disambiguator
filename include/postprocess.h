2c97cdf390d99bf27cafb0c26a96f1996fdc352e
Dave Doolin <david.doolin@gmail.com>
2012-09-01 20:03:39 -0700

    typedefin to Uid@Uinv

--- include/postprocess.h
+++ include/postprocess.h
@@ -11,26 +11,37 @@ class ClusterSet {
 
 private:
     Cluster_Container consolidated;
-    ClusterSet ( const ClusterSet &);
+    ClusterSet (const ClusterSet &);
 
 public:
-    //ClusterSet & convert_from_ClusterInfo( const Cluster_Info * );
-    const Cluster_Container & get_set() const {return consolidated;}
-    Cluster_Container  & get_modifiable_set() { return consolidated; }
+
+    //ClusterSet & convert_from_ClusterInfo(const Cluster_Info *);
+
+    const Cluster_Container & get_set() const {
+      return consolidated;
+    }
+
+    Cluster_Container  & get_modifiable_set() {
+      return consolidated;
+    }
+
     ClusterSet () {}
-    ~ClusterSet() {}
-    void output_results( const char * ) const;
+
+   ~ClusterSet() {}
+
+    void output_results (const char *) const;
 
     void read_from_file (const char * filename,
-                       const map <string, const Record*> & uid_tree);
+                         const map <string, const Record*> & uid_tree);
 };
 
 
-void post_polish(ClusterSet & m,
-                 map < const Record *, const Record *> & uid2uinv,
-                 const PatentTree & patent_tree,
-                 //const map < const Record *, RecordPList, cSort_by_attrib > & patent_tree,
-                 const string & logfile);
+void post_polish (ClusterSet & m,
+                  Uid2UinvTree & uid2uinv,
+                  //map < const Record *, const Record *> & uid2uinv,
+                  const PatentTree & patent_tree,
+                  //const map < const Record *, RecordPList, cSort_by_attrib > & patent_tree,
+                  const string & logfile);
 
 
 #endif /* PATENT_POSTPROCESS_H */
