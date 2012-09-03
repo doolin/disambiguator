df82133f0b877b21b1ffaecf52d3a96bc60649e5
Dave Doolin <david.doolin@gmail.com>
2012-09-02 18:48:12 -0700

    private record header to help out with unit testing.

---
+++ src/record_private.h
@@ -0,0 +1,9 @@
+#ifndef PATENT_RECORD_PRIVATE_H
+#define PATENT_RECORD_PRIVATE_H
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> parse_column_names (std::string line);
+
+#endif // PATENT_RECORD_PRIVATE_H
