5e20b3592e9aba6483de18a044b7fb03bfabb50f
Dave Doolin <david.doolin@gmail.com>
2012-09-03 14:42:11 -0700

    added similarity counts typedefs

--- include/typedefs.h
+++ include/typedefs.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 
+#include <stdint.h>
 
 typedef std::vector<std::string> Label;
 
@@ -15,6 +16,11 @@ typedef std::vector<unsigned int> SimilarityProfile;
 
 //asgdetail consists of assignee number and its patent counts.
 typedef std::pair<std::string, unsigned int> asgdetail;
+
+// Use these for dealing with SimilarityProfile counts.
+typedef uint32_t sp_count_t;
+typedef sp_count_t m_count_t;
+typedef sp_count_t n_count_t;
 
 //typedef map<const Record *, RecordList, cSort_by_attrib> PatentTree;
 
