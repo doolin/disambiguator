6bd65540def4fb42581b0500eb546352a5ccab44
Dave Doolin <david.doolin@gmail.com>
2012-09-25 13:02:41 -0700

    test for record fetching

---
+++ test/test_fetch_records.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <string.h>
+#include <cppunit/TestCase.h>
+
+// Really good web pages:
+// http://stackoverflow.com/questions/318064/how-do-you-declare-an-interface-in-c
+// http://stackoverflow.com/questions/7182359/template-instantiation-details-of-gcc-and-ms-compilers
+//#include <disambiguation.h>
+//#include <engine.h>
+//#include <attribute.h>
+
+#include "testdata.h"
+#include "colortest.h"
+#include "testutils.h"
+
+
+using std::string;
+using std::cout;
+using std::endl;
+
+
+class FetchRecordTest : public CppUnit::TestCase {
+
+public:
+  FetchRecordTest(std::string name) : CppUnit::TestCase(name) {}
+
+};
+
+
+void
+test_fetch_records() {
+
+  FetchRecordTest * frt = new FetchRecordTest("Testing fetch_records");
+  delete frt;
+
+}
+
+#ifdef test_fetch_records_STANDALONE
+int
+main(int, char **) {
+
+  test_fetch_records();
+
+  return 0;
+}
+#endif
