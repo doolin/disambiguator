592a2cd34096793b6ec5e688d31549c18a1cf31a
Dave Doolin <david.doolin@gmail.com>
2012-09-30 16:53:22 -0700

    loading fake data for clustering tests

--- test/test_cluster.cpp
+++ test/test_cluster.cpp
@@ -11,12 +11,20 @@
 #include <cluster.h>
 
 #include "testdata.h"
+#include "testutils.h"
 
 
 class ClusterTest : public CppUnit::TestCase {
 
 public:
-  ClusterTest(std::string name) : CppUnit::TestCase(name) {}
+  ClusterTest(std::string name) : CppUnit::TestCase(name) {
+    describe_test(INDENT0, name.c_str());
+  }
+
+  void load_fake_data() {
+
+    describe_test(INDENT2, "Loading fake data for clustering...");
+  }
 
   void create_cluster() {
     Attribute::register_class_names(get_column_names());
@@ -31,6 +39,7 @@ public:
 
 
   void runTest() {
+    load_fake_data();
     create_cluster();
   }
 };
--- test/test_clusterinfo.cpp
+++ test/test_clusterinfo.cpp
@@ -12,12 +12,35 @@
 #include <cluster.h>
 
 #include "testdata.h"
+#include "testutils.h"
 
 
 class ClusterInfoTest : public CppUnit::TestCase {
 
+private:
+
+  list<Record> source;
+  vector<string> requested_columns;
+
 public:
-  ClusterInfoTest(std::string name) : CppUnit::TestCase(name) {}
+  ClusterInfoTest(std::string name) : CppUnit::TestCase(name) {
+    describe_test(INDENT0, name.c_str());
+  }
+
+  void load_fake_data() {
+
+    describe_test(INDENT2, "Loading fake data for clustering...");
+
+    const char * filename = "testdata/clustertest.csv";
+    requested_columns.push_back(string("Firstname"));
+    requested_columns.push_back(string("Lastname"));
+    requested_columns.push_back(string("Middlename"));
+    bool successful = fetch_records_from_txt(source, filename, requested_columns);
+
+    if (not successful)
+      exit(-1);
+  }
+
 
   void create_cluster() {
     Attribute::register_class_names(get_column_names());
@@ -32,6 +55,7 @@ public:
 
 
   void runTest() {
+    load_fake_data();
     create_cluster();
   }
 };
