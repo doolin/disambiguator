9164dfbcd89bee02326c6282f6d3930c4dbf84ef
Dave Doolin <david.doolin@gmail.com>
2012-09-30 14:50:52 -0700

    much clean up of commenting in cluster info header

--- include/clusterinfo.h
+++ include/clusterinfo.h
@@ -24,71 +24,95 @@
  * Private:
  *
  * const map <string, const Record*> * const uid2record_pointer: 
- *     The binary tree map.
- *     Key   = string of unique record id
- *     Value = the record pointer whose unique record id is the Key.
+ * The binary tree map.
+ * Key   = string of unique record id
+ * Value = the record pointer whose unique record id is the Key.
+ */
 
+/**
  * const bool is_matching:
- *     a boolean indicating the status of the "this" object.
- *     Now it is always set to true.
- *     However, for future expandability, the variable is still kept.
+ * a boolean indicating the status of the "this" object.
+ * Now it is always set to true.
+ * However, for future expandability, the variable is still kept.
+ */
 
+/**
  * uint32_t total_num:
- *     total_number of records. It is used to verify the completeness of "this" object.
- *
+ * total_number of records. It is used to verify the completeness of "this" object.
+ */
+
+/**
  * map < string, cRecGroup > cluster_by_block:
- *     the binary tree map.
- *     Key = blocking identifier.
- *     Value = a list of clusters whose delegates' extracted information blocking string = Key.
- *
+ * the binary tree map.
+ * Key = blocking identifier.
+ * Value = a list of clusters whose delegates' extracted information blocking string = Key.
+ */
+
+/**
  * vector < map < string, uint32_t > > column_stat:
- *     a vector of map:
- *     Key   = extracted string part by a Blocking_Operation object,
- *     Value = its occurrence. The vector stores the statistics for
- *     all the separate parts by the blocking_operation object.
- *
+ * a vector of map:
+ * Key   = extracted string part by a Blocking_Operation object,
+ * Value = its occurrence. The vector stores the statistics for
+ * all the separate parts by the blocking_operation object.
+ */
+
+/**
  * map < const string *, list <double>  > prior_data:
- *     a binary map used to keep track of prior values.
- *     Key = pointer to the blocking id.
- *     Value = a list of double numbers that records the history of priors.
+ * a binary map used to keep track of prior values.
+ * Key = pointer to the blocking id.
+ * Value = a list of double numbers that records the history of priors.
  */
 
 /**
  * map < const string *, bool > block_activity:
- *     a binary map used to check whether disambiguation
- *     within a certain block should occur.
- *     Key = pointer to the block id.
- *     Value = whether or not to disambiguate.
- *
- * vector < uint32_t > max_occurrence:
- *     data collected from column_stat that stores the
- *     maximum occurrence of each blocking string part.
+ * a binary map used to check whether disambiguation
+ * within a certain block should occur.
+ * Key = pointer to the block id.
+ * Value = whether or not to disambiguate.
  */
 
 /**
  * vector < uint32_t > max_occurrence:
- *     data collected from column_stat that stores the
- *     minimum occurrence of each blocking string part.
- *
+ * data collected from column_stat that stores the
+ * maximum occurrence of each blocking string part.
+ */
+
+/**
+ * vector < uint32_t > max_occurrence:
+ * data collected from column_stat that stores the
+ * minimum occurrence of each blocking string part.
+ */
+
+/**
  * string useless:
- *     a string that stores only the delimiters of Blocking_Operation objects.
- *     In other words, blocking id of such string has no information at all.
- *
+ * a string that stores only the delimiters of Blocking_Operation objects.
+ * In other words, blocking id of such string has no information at all.
+ */
+
+/**
  * const bool frequency_adjust_mode:
- *     whether to use frequency to adjust the prior of a certain block.
- *
+ * whether to use frequency to adjust the prior of a certain block.
+ */
+
+/**
  * const bool debug_mode:
  *     debug mode. In debug mode, only certain blocks will be
  *     disambiguated, and the progress is printed.
- *
+ */
+
+/**
  * vector < double > thresholds:
  *     thresholds. The disambiguation is running in a progressive way,
  *     so the threshold are expected to be
  *     like [ 0.99, 0.98, 0.95, ...] in a descending manner.
- *
+ */
+
+/**
  *class cException_Cluster_Error:
  *    exception type used in ClusterInfo.
- *
+ */
+
+/**
  *void config_prior():
  *    config the priori probabilities.
  */
@@ -139,51 +163,6 @@
  *            Returns the number of active blocks. This is designed for debugging.
  */
 
-
-/*
- * Public:
- *        static const char * const primary_delim:
- *            the primary delimiter in the text file that saves disambiguation results.
- *
- *        static const char * const secondary_delim:
- *            the secondary delimiter in the text file that saves disambiguation results.
- *
- *        void output_current_comparision_info(const char * const outputfile ) const:
- *            output current comparison results to a file.
- *
- *        void reset_blocking(const cBlocking_Operation & blocker, const char * const past_comparision_file):
- *            Read the data from the specified file, and use the blocker to
- *            create blocks. A necessary step for disambiguation.
- *
- *        void preliminary_consolidation(const cBlocking_Operation & blocker, const list < const Record *> & all_rec_list):
- *                A preliminary consolidation step. Put all the records with identical blocking
- *                string together in same clusters. In practice, it is used to group records
- *                with exact firstname, middlename, lastname, assignee, city, country together.
- *
- *        ClusterInfo(const map <string, const Record*> & input_uid2record,
- *                      const bool input_is_matching,
- *                      const bool aum,
- *                      const bool debug):
- *                Constructor of a class object.
- *                input_uid2record = map of unique record id string to its Record object pointer.
- *                input_is_matching = whether the object reads a match file.
- *                Always set to true unless new functionalities are added.
- *                aum = frequency adjust mode: on or off.
- *                debug = debug mode: on or off.
- *
- *        void disambiguate(const cRatios & ratiosmap,
- *                          const uint32_t num_threads,
- *                          const char * const debug_block_file,
- *                          const char * const prior_to_save):
- *                The function that starts the disambiguation.
- *                ratiosmap = a cRatios object.
- *                num_threads = number of threads. For maximum performance it is recommended
- *                to be number of CPUs in the machine.
- *                debug_block_file = the file from which specific blocking ids are read and
- *                set active. If it is an invalid file, it is ignored.
- *                prior_to_save = the file to which the priori probability history of each
- *                block will be saved. Perfect for analysis and debugging.
- */
 
 
 /*
@@ -236,7 +215,7 @@ class ClusterInfo {
 
 public:
     typedef set<const Record *> recordset;
-    typedef list < Cluster > cRecGroup;
+    typedef list<Cluster> cRecGroup;
 
     friend bool disambiguate_wrapper(const map<string, ClusterInfo::cRecGroup>::iterator & p,
                                      ClusterInfo & cluster,
@@ -269,26 +248,33 @@ private:
     void config_prior();
 
     uint32_t disambiguate_by_block (cRecGroup & to_be_disambiged_group,
-                                        list <double> & prior_value,
-                                        const cRatios & ratiosmap,
-                                        const string * const bid,
-                                        const double threshold) ;
+                                    list <double> & prior_value,
+                                    const cRatios & ratiosmap,
+                                    const string * const bid,
+                                    const double threshold) ;
 
-    void retrieve_last_comparision_info ( const cBlocking_Operation & blocker, const char * const past_comparision_file);
+    void retrieve_last_comparision_info (const cBlocking_Operation & blocker,
+                                         const char * const past_comparision_file);
 
-    void output_prior_value( const char * const prior_to_save ) const;
+    void output_prior_value(const char * const prior_to_save) const;
 
-    ClusterInfo ( const ClusterInfo &);
+    ClusterInfo (const ClusterInfo &);
 
     void print(std::ostream & os) const;
 
-    const string & get_useless_string() const {return useless;}
+    const string & get_useless_string() const {
+      return useless;
+    }
 
-    double get_prior_value( const string & block_identifier, const list <Cluster> & rg );
+    double get_prior_value(const string & block_identifier, const list <Cluster> & rg );
 
-    const map < const string *, list<double> > & get_prior_map() const {return prior_data;}
+    const map<const string *, list<double> > & get_prior_map() const {
+      return prior_data;
+    }
 
-    map < const string*, list <double> > & get_prior_map() {return prior_data;};
+    map<const string*, list<double> > & get_prior_map() {
+      return prior_data;
+    };
 
     const cRecGroup & get_comparision_map(const string* bid) const;
 
@@ -298,25 +284,75 @@ private:
 
     void debug_disambiguation_loop(cRecGroup::iterator, cRecGroup::iterator, const double, const ClusterHead &);
 
+
 public:
 
+/**
+ * static const char * const primary_delim:
+ * the primary delimiter in the text file that saves disambiguation results.
+ */
     static const char * const primary_delim;
 
+/**
+ * static const char * const secondary_delim:
+ * the secondary delimiter in the text file that saves disambiguation results.
+ */
     static const char * const secondary_delim;
 
     ~ClusterInfo() {}
 
+/**
+ * void output_current_comparision_info(const char * const outputfile ) const:
+ *     output current comparison results to a file.
+ */
     void output_current_comparision_info(const char * const outputfile ) const;
 
+/**
+ * void reset_blocking(const cBlocking_Operation & blocker, const char * const past_comparision_file):
+ *     Read the data from the specified file, and use the blocker to
+ *     create blocks. A necessary step for disambiguation.
+ */
     void reset_blocking(const cBlocking_Operation & blocker, const char * const past_comparision_file);
 
+/**
+ * void preliminary_consolidation(const cBlocking_Operation & blocker, const list < const Record *> & all_rec_list):
+ *  A preliminary consolidation step. Put all the records with identical blocking
+ *  string together in same clusters. In practice, it is used to group records
+ *  with exact firstname, middlename, lastname, assignee, city, country together.
+ */
     void preliminary_consolidation(const cBlocking_Operation & blocker, const list < const Record *> & all_rec_list);
 
+/**
+ * ClusterInfo(const map <string, const Record*> & input_uid2record,
+ * const bool input_is_matching,
+ * const bool aum,
+ * const bool debug):
+ *  Constructor of a class object.
+ *  input_uid2record = map of unique record id string to its Record object pointer.
+ *  input_is_matching = whether the object reads a match file.
+ *  Always set to true unless new functionalities are added.
+ *  aum = frequency adjust mode: on or off.
+ *  debug = debug mode: on or off.
+ */
     ClusterInfo(const map <string, const Record * > & input_uid2record,
                 const bool input_is_matching ,
                 const bool aum ,
                 const bool debug);
 
+   /**
+    * void disambiguate(const cRatios & ratiosmap,
+    *                   const uint32_t num_threads,
+    *            const char * const debug_block_file,
+    *                   const char * const prior_to_save):
+    *  The function that starts the disambiguation.
+    *  ratiosmap = a cRatios object.
+    *  num_threads = number of threads. For maximum performance it is recommended
+    *  to be number of CPUs in the machine.
+    *  debug_block_file = the file from which specific blocking ids are read and
+    *  set active. If it is an invalid file, it is ignored.
+    *  prior_to_save = the file to which the priori probability history of each
+    *  block will be saved. Perfect for analysis and debugging.
+    */
     void disambiguate(const cRatios & ratiosmap,
                       const uint32_t num_threads,
                       const char * const debug_block_file,
