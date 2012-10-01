e25fa122fbf42da3bc7c628aab7fbed3563209fd
Dave Doolin <david.doolin@gmail.com>
2012-09-30 20:00:26 -0700

    get_prior_value unit test now compiles and runs. Not sure if it's running correctly.

--- test/test_clusterinfo.cpp
+++ test/test_clusterinfo.cpp
@@ -91,10 +91,12 @@ public:
 
     Attribute::register_class_names(get_column_names());
     Record * r = make_quuxalot_record();
-    ClusterHead ch(r, 0.9953);
-    RecordPList rl = get_record_list();
+
+    //ClusterHead ch(r, 0.9953);
+    //RecordPList rl = get_record_list();
     cBlocking_Operation_By_Coauthors  blocker_coauthor = get_blocker_coathor();
     Cluster::set_reference_patent_tree_pointer(blocker_coauthor.get_patent_tree());
+
     //Cluster * c = new Cluster(ch, rl);
     //Cluster * c = new Cluster(ch, record_pointers);
     //delete c;
@@ -104,15 +106,37 @@ public:
 
     describe_test(INDENT2, "Testing get_initial_prior");
 
-    ClusterInfo::ClusterList rg;
-    double prior = get_initial_prior(rg, false);
+    Record * r2 = rpv[2];
+    Record * r3 = rpv[3];
+    Record * r4 = rpv[4];
+    //r4->print();
 
+    ClusterHead ch(r2, 0.9953);
+    RecordPList rpl1;
+    rpl1.push_back(r2);
+
+    RecordPList rpl2;
+    rpl2.push_back(r3);
+    rpl2.push_back(r4);
+
+    Cluster c1 = Cluster(ch, rpl1);
+    Cluster c2 = Cluster(ch, rpl2);
+
+    ClusterInfo::ClusterList rg;
+
+    rg.push_back(c1);
+    rg.push_back(c2);
+
+    double prior = get_initial_prior(rg, false);
     std::cout << "prior: " << prior << std::endl;
+
+    CPPUNIT_ASSERT ((0.333333-prior) < 0.00001);
+
   }
 
   void runTest() {
     load_fake_data();
-    create_cluster();
+    //create_cluster();
     test_get_initial_prior();
   }
 };
