066615a9749e634a5cf091561453321d03f17054
Dave Doolin <david.doolin@gmail.com>
2012-10-02 15:17:40 -0700

    ratiocomponent unit test now compiles and links, no tests yet added.

--- test/test_ratiocomponent.cpp
+++ test/test_ratiocomponent.cpp
@@ -12,25 +12,45 @@
 
 #include "testdata.h"
 #include "testutils.h"
-
+#include "fake.h"
 
 class RatioComponentTest : public CppUnit::TestCase {
 
-//private:
+private:
+    cRatioComponent * rc;
+    FakeTest * ft;
 
 public:
 
   RatioComponentTest(std::string name) : CppUnit::TestCase(name) {
+
     describe_test(INDENT0, name.c_str());
+    const string filename("testdata/clustertest.csv");
+    ft = new FakeTest(string("Fake RatioComponentTest"), filename);
+    RecordIndex * uid_dict = ft->get_uid_dict();
+    rc = new cRatioComponent(*uid_dict, string("Patent"));
+  }
+
+ ~RatioComponentTest() {
+    delete ft;
   }
 
   void load_fake_data() {
-
+    const string filename("testdata/clustertest.csv");
+    ft->load_fake_data(filename);
   }
 
+  void test_create_ratios() {
+
+    // Create x_counts and m_counts
+    // maybe clear ratio_map.
+    // call rc->create_ratios();
+    // Find a way to test the result of that call.
+  }
 
   void runTest() {
     load_fake_data();
+    test_create_ratios();
   }
 };
 
@@ -38,7 +58,9 @@ public:
 void
 test_ratiocomponent() {
 
-  RatioComponentTest * rt = new RatioComponentTest(std::string("RatioComponent unit tests"));
+  string title = "Fake object for RatioComponent";
+  string csvfilename = "";
+  RatioComponentTest * rt = new RatioComponentTest(string("RatioComponent test"));
   rt->runTest();
   delete rt;
 }
