bc07b8a0b4d3ea914bec1105a00cea2ad5888c03
Dave Doolin <david.doolin@gmail.com>
2012-10-01 19:11:12 -0700

    added integration test for tset, refactored

--- test/integration.rb
+++ test/integration.rb
@@ -2,16 +2,51 @@
 
 require './termcolors'
 
-print "## #{@colors[116]} Integration testing for priors", RESET, "\n"
+class IntegrationTest
 
-testdir      = "/data/patentdata/disambiguation/experiments/earth/berkeley"
-finalfile    = "./data/berkeley/final_berkeley.txt"
-rarenamefile = "./data/berkeley/rare_names_berkeley.txt"
+  include TermColors
 
-(1..6).each_with_index do |num|
-  testfile  = "#{testdir}/prior_saved_#{num}.txt"
-  priorfile = "./data/berkeley/prior_saved_#{num}.txt"
-  results = `diff -y --suppress-common-lines #{testfile} #{priorfile}`
-  puts results unless results == ""
+  #@@finalfile    = "./data/berkeley/final_berkeley.txt"
+  #@@rarenamefile = "./data/berkeley/rare_names_berkeley.txt"
+
+  @@testdir      = "/data/patentdata/disambiguation/experiments/earth/berkeley"
+
 end
 
+
+class Prior < IntegrationTest
+
+  def initialize
+    print "## #{@colors[116]} Integration testing for priors", RESET, "\n"
+  end
+
+  def test_priors
+    (1..6).each_with_index do |num|
+      testfile  = "#{@@testdir}/prior_saved_#{num}.txt"
+      priorfile = "./data/berkeley/prior_saved_#{num}.txt"
+      results = `diff -y --suppress-common-lines #{testfile} #{priorfile}`
+      puts results unless results == ""
+    end
+  end
+end
+
+class Tset < IntegrationTest
+
+  def initialize
+    print "## #{@@colors[116]} Integration testing for tsets", RESET, "\n"
+  end
+
+  def test_tsets
+    (2..2).each_with_index do |num|
+      testfile  = "#{@@testdir}/tset05_#{num}.txt"
+      priorfile = "./data/berkeley/tset05_#{num}.txt"
+      results = `diff -y --suppress-common-lines #{testfile} #{priorfile}`
+      puts results unless results == ""
+    end
+  end
+
+end
+
+
+tset = Tset.new
+tset.test_tsets
