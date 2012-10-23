733ccae1cd7c2c565847d27c57cf9e7317e73107
Dave Doolin <david.doolin@gmail.com>
2012-10-23 15:26:34 -0700

    added pass, fail and desc display functions

--- test/testutils.cpp
+++ test/testutils.cpp
@@ -5,8 +5,26 @@
 
 
 void
-describe_test(const char * indent, const char * description) {
+describe(const char * indent, const char * description, const char * COLOR) {
 
-    std::cout << indent << DESCCOLOR << description << COLOR_RESET
+    std::cout << indent << COLOR << description << COLOR_RESET
               << std::endl;
 }
+
+void
+describe_test(const char * indent, const char * description) {
+  describe(indent, description, DESCCOLOR);
+}
+
+
+void
+describe_pass(const char * indent, const char * description) {
+  describe(indent, description, PASSCOLOR);
+}
+
+
+void
+describe_fail(const char * indent, const char * description) {
+  describe(indent, description, FAILCOLOR);
+}
+
