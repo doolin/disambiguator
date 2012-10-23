ce0e236dcedb10c91930e698238c07ce44078891
Dave Doolin <david.doolin@gmail.com>
2012-10-23 10:02:31 -0700

    reworked colors for description, pass and fail

--- test/testutils.cpp
+++ test/testutils.cpp
@@ -3,17 +3,10 @@
 #include "colortest.h"
 #include "testutils.h"
 
-/*
-#undef TESTCOLOR
-#define TESTCOLOR ""
-
-#undef COLOR_RESET
-#define COLOR_RESET ""
-*/
 
 void
 describe_test(const char * indent, const char * description) {
 
-    std::cout << indent << TESTCOLOR << description << COLOR_RESET
+    std::cout << indent << DESCCOLOR << description << COLOR_RESET
               << std::endl;
 }
--- test/testutils.h
+++ test/testutils.h
@@ -1,12 +1,17 @@
 #ifndef PATENT_TESTUTILS_H
 #define PATENT_TESTUTILS_H
 
-#define TESTCOLOR COLOR119
+#define DESCCOLOR COLOR45
+#define PASSCOLOR COLOR119
+#define FAILCOLOR COLOR124
+
 #define INDENT0 ""
 #define INDENT2 "  "
 #define INDENT4 "     "
 
 void describe_test(const char * indent, const char * description);
+void describe_pass(const char * indent, const char * description);
+void describe_fail(const char * indent, const char * description);
 
 class TestUtils {
 };
