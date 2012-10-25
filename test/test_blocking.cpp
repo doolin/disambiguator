c3356a83acd764b2520e276b16beba7bf59ffe29
Dave Doolin <david.doolin@gmail.com>
2012-10-25 10:37:31 -0700

    unit test for blocking now in play

--- test/test_blocking.cpp
+++ test/test_blocking.cpp
@@ -8,6 +8,7 @@
 #include <strcmp95.h>
 #include <txt2sqlite3.h>
 
+#include "testutils.h"
 
 #if  0
 const list<const Record *>  psource(new Record()); 
@@ -19,32 +20,41 @@ cBlocking::cBlocking (const list<const Record *> & psource,
 						const vector<string> & blocking_column_names,
 						const vector<const cString_Manipulator*>& pmanipulators,
 						const string & unique_identifier);
+#endif
 
-class BlockingTest : public CppUnit::TestCase { 
 
-public: 
-  BlockingTest( std::string name ) : CppUnit::TestCase( name ) {}
+class BlockingTest : public CppUnit::TestCase {
+
+public:
+  BlockingTest(std::string name) : CppUnit::TestCase(name) {
+    describe_test(INDENT0, name.c_str());
+  }
 
   void delete_blocking() {
-    cBlocking * rc = new cBlocking();
-    delete rc;
+    //cBlocking * rc = new cBlocking();
+    //delete rc;
   }
 
   void runTest() {
-    // Just o get startes...
-    CPPUNIT_ASSERT( 1  == 1 );
     delete_blocking();
   }
 };
-#endif
+
+void
+test_blocking() {
+
+  BlockingTest * bt = new BlockingTest(std::string("Blocking test..."));
+  bt->runTest();
+  delete bt;
+}
+
+
 
 #ifdef test_blocking_STANDALONE
 int
 main(int UP(argc), char ** UP(argv)) {
 
-  //BlockingTest * bt = new BlockingTest(std::string("initial test"));
-  //bt->runTest();
-  //delete bt;
+  test_blocking();
   return 0;
 }
 #endif
