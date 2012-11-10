85f28302dfa0b61fb2b58555cee89742efccf0c9
Dave Doolin <david.doolin@gmail.com>
2012-11-10 09:42:59 -0800

    more rspec style testing

--- test/test_ratios.cpp
+++ test/test_ratios.cpp
@@ -60,16 +60,6 @@ public:
     totalnodes = compute_total_nodes(min, max);
     sprintf(buffer, teststr, totalnodes);
 
-    /*
-    try {
-      CPPUNIT_ASSERT(4 == totalnodes);
-      describer = describe_pass;
-    } catch (CppUnit::Exception e) {
-      describer = describe_fail;
-    }
-    describer(INDENT4, buffer);
-    */
-
     describer = it([totalnodes] () -> bool {
         return (4 == totalnodes);
     });
@@ -80,12 +70,20 @@ public:
     min.push_back(0);
     totalnodes = compute_total_nodes(min, max);
     sprintf(buffer, teststr, totalnodes);
+
+    /*
     try {
       CPPUNIT_ASSERT(12 == totalnodes);
       describer = describe_pass;
     } catch (CppUnit::Exception e) {
       describer = describe_fail;
     }
+    describer(INDENT4, buffer);
+    */
+
+    describer = it([totalnodes] () -> bool {
+        return (12 == totalnodes);
+    });
     describer(INDENT4, buffer);
 
   }
