b46330ff7cc901f38806c34f5b43eb2395a7f986
Dave Doolin <david.doolin@gmail.com>
2012-11-10 21:48:27 -0800

    cleaning up the sh script which is running the tests

--- test/testall.sh
+++ test/testall.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-
 source ./bashcolors.sh
 initializeANSI
 
 ./attribute
-
 ./engine
 ./jwcmp
 ./ratios
@@ -16,40 +14,30 @@ initializeANSI
 ./assigneecomparison
 ./coauthor
 ./strcmp95
-
 ./stringmanipulator
-
-echo "Dis? test"
-`./distest`
-
-# engineconfig spews a huge list of records
-# to stdout. 
-#`engineconfig`
-
-echo "Rare names test"
-`./rarenames`
-
-echo "Typedefs test"
-`./typedefs`
-
-echo "Abbreviations test"
-`./abbreviation`
-
-echo "is_misspell test"
-`./misspell`
-
-echo "name_compare test"
-`./namecompare`
-
-
 ./training
 ./clusterinfo
 ./fetchrecords
 
+
+echo "Dis? test"
+`./distest`
+# engineconfig spews a huge list of records
+# to stdout.
+echo "Engine config"
+./engineconfig
+echo "Rare names test"
+`./rarenames`
+echo "Typedefs test"
+`./typedefs`
+echo "Abbreviations test"
+`./abbreviation`
+echo "is_misspell test"
+`./misspell`
+echo "name_compare test"
+`./namecompare`
 echo "Record test"
 `./record`
-
-
 
 source ./integration.sh
 
