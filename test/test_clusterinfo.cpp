df5e8a1bb4da6500758fa02e16f0ae1e0d9c8f57
Dave Doolin <david.doolin@gmail.com>
2012-11-11 10:21:03 -0800

    cleaning up the clusterinfo tests

--- test/test_clusterinfo.cpp
+++ test/test_clusterinfo.cpp
@@ -16,17 +16,13 @@
 #include "fake.h"
 
 
+
 class ClusterInfoTest : public CppUnit::TestCase {
 
 private:
 
   FakeTest * ft;
-
-  list<Record> source;
-  vector<string> requested_columns;
   RecordPList recpointers;
-  // Accessory container for unit testing, not
-  // present in the disambiguation code.
   vector<const Record *> rpv;
 
 
@@ -40,26 +36,8 @@ public:
     ft->load_fake_data(filename);
     recpointers = ft->get_recpointers();
     rpv = ft->get_recvecs();
-
   }
 
-
-  void create_cluster() {
-
-    describe_test(INDENT2, "Creating clusters...");
-
-    Attribute::register_class_names(get_column_names());
-    Record * r = make_quuxalot_record();
-
-    //ClusterHead ch(r, 0.9953);
-    //RecordPList rl = get_record_list();
-    cBlocking_Operation_By_Coauthors  blocker_coauthor = get_blocker_coathor();
-    Cluster::set_reference_patent_tree_pointer(blocker_coauthor.get_patent_tree());
-
-    //Cluster * c = new Cluster(ch, rl);
-    //Cluster * c = new Cluster(ch, record_pointers);
-    //delete c;
-  }
 
   void test_get_initial_prior() {
 
@@ -68,35 +46,32 @@ public:
     const Record * r2 = rpv[2];
     const Record * r3 = rpv[3];
     const Record * r4 = rpv[4];
-    //r4->print();
+    double chinit = 0.2343;
+    //ClusterHead ch(r2, 0.9953);
+    ClusterHead ch(r2, chinit);
 
-    ClusterHead ch(r2, 0.9953);
-    RecordPList rpl1;
-    rpl1.push_back(r2);
-
-    RecordPList rpl2;
-    rpl2.push_back(r3);
-    rpl2.push_back(r4);
-
+    RecordPList rpl1 = { r2 };
     Cluster c1 = Cluster(ch, rpl1);
+
+    RecordPList rpl2 = { r3, r4 };
     Cluster c2 = Cluster(ch, rpl2);
 
-    ClusterInfo::ClusterList rg;
-
-    rg.push_back(c1);
-    rg.push_back(c2);
+    ClusterInfo::ClusterList rg = { c1, c2 };
 
     double prior = get_initial_prior(rg, false);
-    //std::cout << "prior: " << prior << std::endl;
 
-    CPPUNIT_ASSERT ((0.333333-prior) < 0.00001);
+    Spec spec;
+    spec.it("The prior should be ~0.333333", [&](Description desc)->bool {
+      return ((0.333333-prior) < 0.00001);
+    });
 
   }
 
-  void runTest() {
-    //create_cluster();
+
+  void runTests() {
     test_get_initial_prior();
   }
+
 };
 
 
@@ -104,7 +79,7 @@ void
 test_clusterinfo() {
 
   ClusterInfoTest * cit = new ClusterInfoTest(std::string("ClusterInfo test"));
-  cit->runTest();
+  cit->runTests();
   delete cit;
 }
 
