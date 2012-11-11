b53e52cb47eb357ddeb4fd908eb841411ea02430
Dave Doolin <david.doolin@gmail.com>
2012-11-11 07:50:51 -0800

    misspell function specced

--- test/test_misspell.cpp
+++ test/test_misspell.cpp
@@ -1,60 +1,86 @@
 
 #include <string>
 
-#include <cppunit/Portability.h>
-#include <cppunit/portability/CppUnitSet.h>
-#include <cppunit/extensions/TestFactory.h>
 #include <cppunit/TestCase.h>
 
-
 #include "comparators.h"
+#include "testutils.h"
+
 
 /**
  * Run this code with /usr/bin/valgrind ./missing --leak-check=full
  */
 
-void
-test_is_misspell() {
+class Misspell : public CppUnit::TestCase {
 
-  const char * s1 = "misspell";
-  const char * s2 = "mispell";
-  CPPUNIT_ASSERT (1 == is_misspell(s1,s2));
-}
+public:
+  Misspell(std::string name) : CppUnit::TestCase(name) {
 
-
-void
-test_is_not_misspell() {
-
-  const char * s1 = "misspell";
-  const char * s2 = "misspell";
-  CPPUNIT_ASSERT (4 == is_misspell(s1,s2));
-}
+    describe_test(INDENT0, name.c_str());
+  }
 
 
-void
-test_is_missepll() {
+    void test_is_misspell() {
 
-  const char * s1 = "misspell";
-  const char * s2 = "missepll";
-  CPPUNIT_ASSERT (2 == is_misspell(s1,s2));
-}
+      Spec spec;
+      spec.it("Testing 'misspell' against 'mispell' returns 1", DO_SPEC {
+        const char * s1 = "misspell";
+        const char * s2 = "mispell";
+        return (1 == is_misspell(s1,s2));
+      });
+    }
 
 
-void
-test_is_misspall() {
+    void test_is_not_misspell() {
 
-  const char * s1 = "misspell";
-  const char * s2 = "misspall";
-  CPPUNIT_ASSERT (3 == is_misspell(s1,s2));
-}
+      Spec spec;
+      spec.it("Testing 'misspell' against 'misspell' returns 4", DO_SPEC {
+        const char * s1 = "misspell";
+        const char * s2 = "misspell";
+        return (4 == is_misspell(s1,s2));
+      });
+    }
+
+
+    void test_is_missepll() {
+
+      Spec spec;
+      spec.it("Testing 'misspell' against missepll' returns 2", DO_SPEC {
+        const char * s1 = "misspell";
+        const char * s2 = "missepll";
+        return (2 == is_misspell(s1,s2));
+      });
+    }
+
+
+    void test_is_misspall() {
+
+      Spec spec;
+      spec.it("Testing 'misspell' against 'misspall' returns 3", DO_SPEC {
+        const char * s1 = "misspell";
+        const char * s2 = "misspall";
+        return (3 == is_misspell(s1,s2));
+      });
+    }
+
+
+
+  void runTests() {
+    test_is_misspell();
+    test_is_not_misspell();
+    test_is_missepll();
+    test_is_misspall();
+  }
+
+};
+
 
 
 void
 test_misspell() {
-  test_is_misspell();
-  test_is_not_misspell();
-  test_is_missepll();
-  test_is_misspall();
+  Misspell * mt = new Misspell("Misspell (is_misspell()) function unit test");
+  mt->runTests();
+  delete mt;
 }
 
 #ifdef test_misspell_STANDALONE
